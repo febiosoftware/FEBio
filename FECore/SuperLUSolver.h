b1289f55acba0e828b73714385b427f8c531effe
SteveMaas1978 <steve.maas@utah.edu>
2009-08-20 18:36:59 +0000

    FEBio: for some reason these files where not commited so I'll try it again.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio@300 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FECore/SuperLUSolver.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "LinearSolver.h"
+
+//-----------------------------------------------------------------------------
+//! Implements a wrapper class for the SuperLU library
+
+//! This solver can only be used on systems where it is available.
+//! This solver also uses some of the BLAS routines so this package also needs
+//! to be available on the system. Although SuperLU comes with a stripped down
+//! version of BLAS.
+
+#ifdef SUPERLU
+		#include "slu_ddefs.h"
+#endif
+
+class SuperLUSolver : public LinearSolver
+{
+public:
+	bool PreProcess(SparseMatrix& K);
+	bool Factor(SparseMatrix& K);
+	bool Solve(SparseMatrix& K, vector<double>& x, vector<double>& b);
+	bool Solve(SparseMatrix& K, matrix& x, matrix& b);
+	void Destroy();
+
+	SparseMatrix* GetMatrix(int ntype) { return new CompactUnSymmMatrix(); }
+
+	SuperLUSolver() { m_balloc = false; m_bfact = false; m_bcond = false; }
+
+#ifdef SUPERLU
+protected:
+	double norm(SparseMatrix& K); // calculates the 1-norm of the matrix A
+#endif
+
+protected:
+
+	bool m_balloc;
+	bool m_bfact;
+	bool m_bcond;	// calculate condition numbers
+
+
+#ifdef SUPERLU
+
+	SuperMatrix A, L, U, B, X;
+	vector<int>	perm_c;
+	vector<int>	perm_r;
+	vector<int>	etree;
+
+	superlu_options_t	options;
+	SuperLUStat_t	stat;
+	mem_usage_t	mem_usage;
+
+	double	rpg, rcond;
+	double	ferr, berr;
+	int		info;
+	char	equed[1];
+
+#endif // SUPERLU
+};
