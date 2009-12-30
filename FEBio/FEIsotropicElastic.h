42c4dfca7bd9df178d9a1924297fae5254451090
SteveMaas1978 <steve.maas@utah.edu>
2009-12-30 20:51:08 +0000

    FEBio: added the isotropic elastic material which makes the linear elastic and St.Venant-Kirchhoff material obsolete.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio@601 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FEBio/FEIsotropicElastic.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "FEMaterial.h"
+
+class FEIsotropicElastic : public FEElasticMaterial
+{
+public:
+	FEIsotropicElastic() {}
+
+public:
+	double	m_E;	//!< Young's modulus
+	double	m_v;	//!< Poisson's ratio
+
+public:
+	//! calculate stress at material point
+	virtual mat3ds Stress(FEMaterialPoint& pt);
+
+	//! calculate tangent stiffness at material point
+	virtual tens4ds Tangent(FEMaterialPoint& pt);
+
+	//! data initialization
+	void Init();
+
+	//! return bulk modulus
+	double BulkModulus() { return m_E/(3.0*(1.0 - 2.0*m_v));}
+
+	// declare as registered
+	DECLARE_REGISTERED(FEIsotropicElastic);
+
+	// declare the parameter list
+	DECLARE_PARAMETER_LIST();
+};
