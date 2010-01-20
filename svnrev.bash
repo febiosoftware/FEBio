b7d79f7a713292a2f259463fa37bb01b1ec1d00d
rawlins <user@email>
2010-01-20 22:34:18 +0000

    Changes with makefile and bash files
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio@676 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- svnrev.bash
+++ svnrev.bash
@@ -3,6 +3,7 @@
 # Executing this bash script before compiling FEBio will add the svn revision number to the version
 # number in the FEBio splash screen.  Note that this will only work if FEBio is under svn version control.
 
-svnrev=$(svnversion -n)
+temp=$(svnversion -n)
+svnrev=$( echo $temp | cut -f1 -d'M')
 
 sed s/[$]WCREV[$]/$svnrev/ FEBio/svnrev_tpl.txt > FEBio/svnrev.h
