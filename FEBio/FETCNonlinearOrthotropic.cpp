1b3aca308d8e9e07eb415d22ad5b937bdf43dd15
SteveMaas1978 <steve.maas@utah.edu>
2010-09-01 14:28:43 +0000

    FEBio: rewrote FETCNonlinearOrthotropic::DevStress using tensor arithmetic.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio@1250 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEBio/FETCNonlinearOrthotropic.cpp
+++ FEBio/FETCNonlinearOrthotropic.cpp
@@ -47,17 +47,11 @@ mat3ds FETCNonlinearOrthotropic::DevStress(FEMaterialPoint& mp)
 	double Jm23 = Jm13*Jm13;
 	double twoJi = 2.0*Ji;
 
-	// left Cauchy-Green tensor and its square
-	double B[3][3], B2[3][3];
-
 	// invariants of B
 	double I1, I2, I4a, I4b, I4c;
 
 	// strain energy derivatives
 	double W1, W2, W4a, W4b, W4c;
-
-	// T = F*dW/dC*Ft, trT = trace[T]
-	double T[3][3], trT;
 
 	// current local material axis
 	vec3d a0, b0, c0, a, b, c;
@@ -73,17 +67,9 @@ mat3ds FETCNonlinearOrthotropic::DevStress(FEMaterialPoint& mp)
 	a0.z = pt.Q[2][0]; b0.z = pt.Q[2][1]; c0.z = pt.Q[2][2];
 
 	// calculate the current material axes lam*a = F*a0;
-	a.x = F[0][0]*a0.x + F[0][1]*a0.y + F[0][2]*a0.z;
-	a.y = F[1][0]*a0.x + F[1][1]*a0.y + F[1][2]*a0.z;
-	a.z = F[2][0]*a0.x + F[2][1]*a0.y + F[2][2]*a0.z;
-
-	b.x = F[0][0]*b0.x + F[0][1]*b0.y + F[0][2]*b0.z;
-	b.y = F[1][0]*b0.x + F[1][1]*b0.y + F[1][2]*b0.z;
-	b.z = F[2][0]*b0.x + F[2][1]*b0.y + F[2][2]*b0.z;
-
-	c.x = F[0][0]*c0.x + F[0][1]*c0.y + F[0][2]*c0.z;
-	c.y = F[1][0]*c0.x + F[1][1]*c0.y + F[1][2]*c0.z;
-	c.z = F[2][0]*c0.x + F[2][1]*c0.y + F[2][2]*c0.z;
+	a = F*a0;
+	b = F*b0;
+	c = F*c0;
 
 	// normalize material axis and store fiber stretch
 	la  = a.unit();
@@ -95,37 +81,16 @@ mat3ds FETCNonlinearOrthotropic::DevStress(FEMaterialPoint& mp)
 	lc  = c.unit();
 	lct = lc*Jm13;
 
-	// calculate deviatoric left Cauchy-Green tensor
-	B[0][0] = Jm23*(F[0][0]*F[0][0]+F[0][1]*F[0][1]+F[0][2]*F[0][2]);
-	B[0][1] = Jm23*(F[0][0]*F[1][0]+F[0][1]*F[1][1]+F[0][2]*F[1][2]);
-	B[0][2] = Jm23*(F[0][0]*F[2][0]+F[0][1]*F[2][1]+F[0][2]*F[2][2]);
+	// get deviatoric left Cauchy-Green tensor
+	mat3ds B = pt.DevLeftCauchyGreen();
 
-	B[1][0] = Jm23*(F[1][0]*F[0][0]+F[1][1]*F[0][1]+F[1][2]*F[0][2]);
-	B[1][1] = Jm23*(F[1][0]*F[1][0]+F[1][1]*F[1][1]+F[1][2]*F[1][2]);
-	B[1][2] = Jm23*(F[1][0]*F[2][0]+F[1][1]*F[2][1]+F[1][2]*F[2][2]);
-
-	B[2][0] = Jm23*(F[2][0]*F[0][0]+F[2][1]*F[0][1]+F[2][2]*F[0][2]);
-	B[2][1] = Jm23*(F[2][0]*F[1][0]+F[2][1]*F[1][1]+F[2][2]*F[1][2]);
-	B[2][2] = Jm23*(F[2][0]*F[2][0]+F[2][1]*F[2][1]+F[2][2]*F[2][2]);
-
-	// calculate square of B
-	// (we commented out the matrix components we do not need)
-	B2[0][0] = B[0][0]*B[0][0]+B[0][1]*B[1][0]+B[0][2]*B[2][0];
-	B2[0][1] = B[0][0]*B[0][1]+B[0][1]*B[1][1]+B[0][2]*B[2][1];
-	B2[0][2] = B[0][0]*B[0][2]+B[0][1]*B[1][2]+B[0][2]*B[2][2];
-
-//	B2[1][0] = B[1][0]*B[0][0]+B[1][1]*B[1][0]+B[1][2]*B[2][0];
-	B2[1][1] = B[1][0]*B[0][1]+B[1][1]*B[1][1]+B[1][2]*B[2][1];
-	B2[1][2] = B[1][0]*B[0][2]+B[1][1]*B[1][2]+B[1][2]*B[2][2];
-
-//	B2[2][0] = B[2][0]*B[0][0]+B[2][1]*B[1][0]+B[2][2]*B[2][0];
-//	B2[2][1] = B[2][0]*B[0][1]+B[2][1]*B[1][1]+B[2][2]*B[2][1];
-	B2[2][2] = B[2][0]*B[0][2]+B[2][1]*B[1][2]+B[2][2]*B[2][2];
+	// square of B
+	mat3ds B2 = B*B;
 
 	// Invariants of B (= invariants of C)
 	// Note that these are the invariants of Btilde, not of B!
-	I1 = B[0][0]+B[1][1]+B[2][2];
-	I2 = 0.5*(I1*I1 - ( B2[0][0] + B2[1][1] + B2[2][2]) );
+	I1 = B.tr();
+	I2 = 0.5*(I1*I1 - B2.tr());
 	I4a = lat*lat;
 	I4b = lbt*lbt;
 	I4c = lct*lct;
@@ -179,32 +144,13 @@ mat3ds FETCNonlinearOrthotropic::DevStress(FEMaterialPoint& mp)
 	// (we commented out the matrix components we do not need)
 	w1pw2i1 = W1 + W2*I1; // TODO: is there a term missing ???? (W4*I4 for a, b and c ??)
 
-	T[0][0] = w1pw2i1*B[0][0] - W2*B2[0][0] + W4a*I4a*a.x*a.x + W4b*I4b*b.x*b.x + W4c*I4c*c.x*c.x;
-	T[0][1] = w1pw2i1*B[0][1] - W2*B2[0][1] + W4a*I4a*a.x*a.y + W4b*I4b*b.x*b.y + W4c*I4c*c.x*c.y;
-	T[0][2] = w1pw2i1*B[0][2] - W2*B2[0][2] + W4a*I4a*a.x*a.z + W4b*I4b*b.x*b.z + W4c*I4c*c.x*c.z;
+	mat3ds AxA = dyad(a);
+	mat3ds BxB = dyad(b);
+	mat3ds CxC = dyad(c);
 
-//	T[1][0] = w1pw2i1*B[1][0] - W2*B2[1][0] + W4a*I4a*a.y*a.x + W4b*I4b*b.y*b.x + W4c*I4c*c.y*c.x;
-	T[1][1] = w1pw2i1*B[1][1] - W2*B2[1][1] + W4a*I4a*a.y*a.y + W4b*I4b*b.y*b.y + W4c*I4c*c.y*c.y;
-	T[1][2] = w1pw2i1*B[1][2] - W2*B2[1][2] + W4a*I4a*a.y*a.z + W4b*I4b*b.y*b.z + W4c*I4c*c.y*c.z;
+	mat3ds T = B*w1pw2i1 - B2*W2 + AxA*(W4a*I4a) + BxB*(W4b*I4b) + CxC*(W4c*I4c);
 
-//	T[2][0] = w1pw2i1*B[2][0] - W2*B2[2][0] + W4a*I4a*a.z*a.x + W4b*I4b*b.z*b.x + W4c*I4c*c.z*c.x;
-//	T[2][1] = w1pw2i1*B[2][1] - W2*B2[2][1] + W4a*I4a*a.z*a.y + W4b*I4b*b.z*b.y + W4c*I4c*c.z*c.y;
-	T[2][2] = w1pw2i1*B[2][2] - W2*B2[2][2] + W4a*I4a*a.z*a.z + W4b*I4b*b.z*b.z + W4c*I4c*c.z*c.z;
-
-	// trace of T/3
-	trT = (T[0][0] + T[1][1] + T[2][2])*third;
-
-	// calculate stress: 
-	mat3ds s;
-
-	s.xx() = twoJi*(T[0][0] - trT);
-	s.yy() = twoJi*(T[1][1] - trT);
-	s.zz() = twoJi*(T[2][2] - trT);
-	s.xy() = twoJi*T[0][1];
-	s.yz() = twoJi*T[1][2];
-	s.xz() = twoJi*T[0][2];
-
-	return s;
+	return T.dev()*(2.0/J);
 }
 
 //-----------------------------------------------------------------------------
