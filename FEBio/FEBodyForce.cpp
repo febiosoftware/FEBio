d2e2ea979e2d4613d394b5282dddd402e06b8532
SteveMaas1978 <steve.maas@utah.edu>
2010-12-17 17:22:20 +0000

    FEBio: committed FEBodyForce.cpp
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio@1557 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FEBio/FEBodyForce.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "FEBodyForce.h"
+
+//-----------------------------------------------------------------------------
+FENonConstBodyForce::FENonConstBodyForce()
+{
+	m_sz[0][0] = 0;
+	m_sz[1][0] = 0;
+	m_sz[2][0] = 0;
+}
+
+//-----------------------------------------------------------------------------
+vec3d FENonConstBodyForce::force(FEMaterialPoint &mp)
+{
+	FEElasticMaterialPoint& pt = *mp.ExtractData<FEElasticMaterialPoint>();
+
+	// get the material point's spatial position
+	vec3d r = pt.rt;
+
+	// define a math parser object
+	MathParser m;
+	m.SetVariable("x", r.x);
+	m.SetVariable("y", r.y);
+	m.SetVariable("z", r.z);
+
+	// calculate the force
+	vec3d f;
+	int ierr;
+	f.x = m.eval(m_sz[0], ierr);
+	f.y = m.eval(m_sz[1], ierr);
+	f.z = m.eval(m_sz[2], ierr);
+	
+	return f;
+}
+
+//-----------------------------------------------------------------------------
+mat3ds FENonConstBodyForce::stiffness(FEMaterialPoint& pt)
+{
+	return mat3ds(0,0,0,0,0,0);
+}
