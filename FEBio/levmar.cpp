61f6188ad49b8fcab0f112f1169a03d4dc153d79
rawlins <user@email>
2011-01-13 21:58:17 +0000

    Changed levmar.cpp back to obj.m_nlc being 0 based
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio@1628 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEBio/levmar.cpp
+++ FEBio/levmar.cpp
@@ -53,7 +53,7 @@ bool FELMOptimizeMethod::Solve(FEOptimizeData *pOpt)
 
 	// set the data
 	OPT_OBJECTIVE& obj = opt.GetObjective();
-	FELoadCurve& lc = opt.GetLoadCurve(obj.m_nlc - 1);
+	FELoadCurve& lc = opt.GetLoadCurve(obj.m_nlc);
 	int ndata = lc.Points();
 	vector<double> x(ndata), y(ndata);
 	for (i=0; i<ndata; ++i) 
