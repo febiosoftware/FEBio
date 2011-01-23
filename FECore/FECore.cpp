8f5bdf945e13015be2a2729f63b52255c8868bbc
SteveMaas1978 <steve.maas@utah.edu>
2011-01-23 23:05:32 +0000

    FECore: made a small change to the FECore namespace.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio@1664 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FECore/FECore.cpp
+++ FECore/FECore.cpp
@@ -1,29 +1,21 @@
 #include "stdafx.h"
 #include "FECore.h"
 
-//-----------------------------------------------------------------------------
-//! The FECore namespace encapsulates all classes that belong to the FECore library
-
-//! The FECore library is a collection of tools that simplify the developement
-//! of FE software. 
-
-namespace FECore
-{
-
 #define FECORE_VERSION		0
 #define FECORE_SUBVERSION	1
 
-static char fecore_str[4] = {'0'+FECORE_VERSION, '.', '0'+FECORE_SUBVERSION };
-
-void get_version(int& version, int& subversion)
+//-----------------------------------------------------------------------------
+// Get the version info
+void FECore::get_version(int& version, int& subversion)
 {
 	version = FECORE_VERSION;
 	subversion = FECORE_SUBVERSION;
 }
 
-const char* get_version_string()
+//-----------------------------------------------------------------------------
+// get the version string
+const char* FECore::get_version_string()
 {
+	static const char fecore_str[4] = {'0'+FECORE_VERSION, '.', '0'+FECORE_SUBVERSION };
 	return fecore_str;
 }
-
-} // namespace FECore
