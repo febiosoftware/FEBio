f90d8568e3e77243f00c3f8281cfd2fa24f8b366
SteveMaas1978 <steve.maas@utah.edu>
2012-02-01 18:03:01 +0000

    FEBio2: updated VS08 solution.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@3085 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FECore/FESurfaceLoad.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "FEBoundaryCondition.h"
+#include "FESurface.h"
+#include "DumpFile.h"
+#include "FENLSolver.h"
+
+//-----------------------------------------------------------------------------
+//! This is the base class for all loads that are applied to surfaces
+class FESurfaceLoad : public FEBoundaryCondition
+{
+public:
+	FESurfaceLoad(FESurface* ps);
+	virtual ~FESurfaceLoad(void);
+
+	//! Get the surface
+	FESurface& Surface() { return *m_psurf; }
+
+	//! calculate stiffness matrix
+	virtual void StiffnessMatrix(FENLSolver* psolver) = 0;
+
+	//! calculate residual
+	virtual void Residual(FENLSolver* psolver, vector<double>& R) = 0;
+
+	//! serialization
+	virtual void Serialize(DumpFile& ar) = 0;
+
+protected:
+	FESurface*	m_psurf;
+};
