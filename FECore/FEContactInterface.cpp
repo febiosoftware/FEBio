981cb4e924269caadd728296a7959ec8fedaba4c
SteveMaas1978 <steve.maas@utah.edu>
2013-03-25 21:51:04 +0000

    FEBio2: fixed bug in contact interface when bulk modulus is calculated for rigid materials.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@4266 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FECore/FEContactInterface.cpp
+++ FECore/FEContactInterface.cpp
@@ -45,28 +45,31 @@ double FEContactInterface::BulkModulus(FESurfaceElement& el, FESurface &s)
 		// extract the elastic material
 		FEElasticMaterial* pme = m_pfem->GetMaterial(pe->GetMatID())->GetElasticMaterial();
 
-		// get a material point
-		FEMaterialPoint& mp = *pe->m_State[0];
-		FEElasticMaterialPoint& pt = *(mp.ExtractData<FEElasticMaterialPoint>());
+		if (pme)
+		{
+			// get a material point
+			FEMaterialPoint& mp = *pe->m_State[0];
+			FEElasticMaterialPoint& pt = *(mp.ExtractData<FEElasticMaterialPoint>());
 
-		// setup the material point
-		pt.F = mat3dd(1.0);
-		pt.J = 1;
-		pt.s.zero();
+			// setup the material point
+			pt.F = mat3dd(1.0);
+			pt.J = 1;
+			pt.s.zero();
 
-		// get the tangent (stiffness) and it inverse (compliance) at this point
-		tens4ds S = pme->Tangent(pt);
-		tens4ds C = S.inverse();
+			// get the tangent (stiffness) and it inverse (compliance) at this point
+			tens4ds S = pme->Tangent(pt);
+			tens4ds C = S.inverse();
 
-		// evaluate element surface normal at parametric center
-		vec3d t[2];
-		s.CoBaseVectors0(el, 0, 0, t);
-		vec3d n = t[0] ^ t[1];
-		n.unit();
+			// evaluate element surface normal at parametric center
+			vec3d t[2];
+			s.CoBaseVectors0(el, 0, 0, t);
+			vec3d n = t[0] ^ t[1];
+			n.unit();
 		
-		// evaluate normal component of the compliance matrix
-		// (equivalent to inverse of Young's modulus along n)
-		eps = 1./(n*(vdotTdotv(n, C, n)*n));
+			// evaluate normal component of the compliance matrix
+			// (equivalent to inverse of Young's modulus along n)
+			eps = 1./(n*(vdotTdotv(n, C, n)*n));
+		}
 	}
 
 	return eps;
