e3d1f7510e3d04ea4236888b1c93bf1c324fbde2
rawlins <user@email>
2013-09-26 20:39:30 +0000

    Updated makefile
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@4916 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEBio2/febio2.mk
+++ FEBio2/febio2.mk
@@ -13,11 +13,15 @@ FEBIOMIX = ../lib/febiomix_$(PLAT).a
 
 FEBIOHEAT = ../lib/febioheat_$(PLAT).a
 
+FEBIOMIX = ../lib/febiomix_$(PLAT).a
+
+FEBIOMECH = ../lib/febiomech_$(PLAT).a
+
 FEBIOXML = ../lib/febioxml_$(PLAT).a
 
 NUMCORE = ../lib/numcore_$(PLAT).a
 
-FEBIO2LIBS = -Wl,--start-group $(FECORE) $(FEBIOLIB) $(FEBIOPLOT) $(FEBIOMECH) $(FEBIOMIX) $(FEBIOHEAT) $(FEBIOXML) $(NUMCORE) -Wl,--end-group
+FEBIO2LIBS = -Wl,--start-group $(FEBIOLIB) $(FEBIOPLOT) $(FEBIOOPT) $(FEBIOHEAT) $(FEBIOMIX) $(FEBIOMECH) $(FEBIOXML) $(FECORE) $(NUMCORE) -Wl,--end-group
 
 $(TARGET):
 	$(CC) -o $(TARGET) $(DEF) *.cpp $(FLG) $(INC) $(FEBIO2LIBS) $(LIBS)
