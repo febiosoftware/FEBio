043b275820e16384b8deb85fbee0be185c1db3b0
SteveMaas1978 <steve.maas@utah.edu>
2013-11-21 17:48:49 +0000

    FEBio2: committed FENLConstraint.cpp.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@5165 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FECore/FENLConstraint.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "FENLConstraint.h"
+
+
+//-----------------------------------------------------------------------------
+FENLConstraint::FENLConstraint(FEModel* pfem) : m_pfem(pfem) {}
+
+//-----------------------------------------------------------------------------
+FENLConstraint::~FENLConstraint(){}
+
+//-----------------------------------------------------------------------------
+bool FENLConstraint::IsActive()
+{ 
+	return m_bactive; 
+}
+
+//-----------------------------------------------------------------------------
+void FENLConstraint::Activate()
+{ 
+	m_bactive = true; 
+}
+
+//-----------------------------------------------------------------------------
+void FENLConstraint::Deactivate()
+{ 
+	m_bactive = false; 
+}
