e75e2f1529606816ce70d1d0d6cf1eede733c134
rawlins <user@email>
2013-12-06 22:22:46 +0000

    FEBio2: Fixed bug in Multiphasic serialization.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@5258 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEBioMix/FEMultiphasic.cpp
+++ FEBioMix/FEMultiphasic.cpp
@@ -306,14 +306,35 @@ void FESolutesMaterialPoint::Serialize(DumpFile& ar)
 	else
 	{
 		ar >> m_nsol >> m_psi >> m_cF >> m_Ie >> m_nsbm;
+
+		m_c.resize(m_nsol);
+		m_gradc.resize(m_nsol);
+		m_j.resize(m_nsol);
+		m_ca.resize(m_nsol);
+		m_k.resize(m_nsol);
+		m_dkdJ.resize(m_nsol);
+		m_dkdc.resize(m_nsol);
+
 		for (int i=0; i<m_nsol; ++i) {
 			ar >> m_c[i] >> m_gradc[i] >> m_j[i] >> m_ca[i]
 			>> m_k[i] >> m_dkdJ[i];
+
+			m_dkdc[i].resize(m_nsol);
 			for (int j=0; j<m_nsol; ++j)
+			{
 				ar >> m_dkdc[i][j];
+			}
 		}
-		for (int i=0; i<m_nsbm; ++i)
-			ar >> m_sbmr[i] >> m_sbmrp[i] >> m_sbmrhat[i];
+		
+		if (m_nsbm)
+		{
+			m_sbmr.resize(m_nsbm);
+			m_sbmrp.resize(m_nsbm);
+			m_sbmrhat.resize(m_nsbm);
+
+			for (int i=0; i<m_nsbm; ++i)
+				ar >> m_sbmr[i] >> m_sbmrp[i] >> m_sbmrhat[i];
+		}
 	}
 		
 	if (m_pt) m_pt->Serialize(ar);
