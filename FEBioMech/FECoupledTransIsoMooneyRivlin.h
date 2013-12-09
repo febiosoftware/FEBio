cff0da1bace81df3ddf2eed73fed2d2cccb8c319
SteveMaas1978 <steve.maas@utah.edu>
2013-12-09 23:53:06 +0000

    FEBio2: Added coupled trans iso MR and VW materials. (forgot to add files)
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@5285 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FEBioMech/FECoupledTransIsoMooneyRivlin.h
@@ -0,0 +1,40 @@
+// FEMooneyRivlin.h: interface for the FEMooneyRivlin class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "FEElasticMaterial.h"
+
+//-----------------------------------------------------------------------------
+//! Coupled transversely-isotropic Mooney-Rivlin material
+//!
+class FECoupledTransIsoMooneyRivlin : public FEElasticMaterial
+{
+public:
+	FECoupledTransIsoMooneyRivlin(FEModel* pfem) : FEElasticMaterial(pfem) {}
+
+public:
+	double	m_c1;	//!< Mooney-Rivlin coefficient C1
+	double	m_c2;	//!< Mooney-Rivlin coefficient C2
+	double	m_c3;	//!< fiber stress scale factor
+	double	m_c4;	//!< exponential scale factor
+	double	m_c5;	//!< slope of linear stress region
+	double	m_flam;	//!< fiber stretch at which fibers are straight
+	double	m_K;	//!< "bulk"-modulus
+
+public:
+	//! calculate deviatoric stress at material point
+	mat3ds Stress(FEMaterialPoint& pt);
+
+	//! calculate deviatoric tangent stiffness at material point
+	tens4ds Tangent(FEMaterialPoint& pt);
+
+	//! data initialization
+	void Init();
+
+	// declare the parameter list
+	DECLARE_PARAMETER_LIST();
+};
---
+++ FEBioMech/FECoupledTransIsoVerondaWestmann.cpp
@@ -0,0 +1,167 @@
+// FEMooneyRivlin.cpp: implementation of the FEMooneyRivlin class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "FECoupledTransIsoVerondaWestmann.h"
+
+//-----------------------------------------------------------------------------
+// define the material parameters
+BEGIN_PARAMETER_LIST(FECoupledTransIsoVerondaWestmann, FEElasticMaterial)
+	ADD_PARAMETER(m_c1  , FE_PARAM_DOUBLE, "c1");
+	ADD_PARAMETER(m_c2  , FE_PARAM_DOUBLE, "c2");
+	ADD_PARAMETER(m_c3  , FE_PARAM_DOUBLE, "c3");
+	ADD_PARAMETER(m_c4  , FE_PARAM_DOUBLE, "c4");
+	ADD_PARAMETER(m_c5  , FE_PARAM_DOUBLE, "c5");
+	ADD_PARAMETER(m_flam, FE_PARAM_DOUBLE, "lambda");
+	ADD_PARAMETER(m_K   , FE_PARAM_DOUBLE, "k");
+END_PARAMETER_LIST();
+
+//-----------------------------------------------------------------------------
+void FECoupledTransIsoVerondaWestmann::Init()
+{
+	if (m_c1 <= 0) throw MaterialError("c1 must be positive");
+	FEElasticMaterial::Init();
+}
+
+//-----------------------------------------------------------------------------
+//! Calculate the Cauchy stress
+mat3ds FECoupledTransIsoVerondaWestmann::Stress(FEMaterialPoint& mp)
+{
+	// get the material point data
+	FEElasticMaterialPoint& pt = *mp.ExtractData<FEElasticMaterialPoint>();
+
+	// calculate left Cauchy-Green tensor
+	mat3ds B = pt.LeftCauchyGreen();
+
+	// calculate square of B
+	mat3ds B2 = B*B;
+
+	// get the material fiber axis
+	vec3d a0;
+	a0.x = pt.m_Q[0][0];
+	a0.y = pt.m_Q[1][0];
+	a0.z = pt.m_Q[2][0];
+
+	// get the spatial fiber axis
+	vec3d a = pt.m_F*a0;
+	double l = a.unit();
+
+	// Invariants of B (= invariants of C)
+	double I1 = B.tr();
+	double J = pt.m_J;
+
+	// some useful tensors
+	mat3dd I(1.0);
+	mat3ds A = dyad(a);
+
+	// a. define the matrix stress
+	//-----------------------------
+	// W = C1*(exp(C2*(I1-3)-1)-0.5*C1*C2*(I2 - 3)
+	// Wi = dW/dIi
+	double W1 = m_c1*m_c2*exp(m_c2*(I1-3));
+	double W2 = -0.5*m_c1*m_c2;
+
+	mat3ds s = (B*(W1 + I1*W2) - B2*W2)*(2.0/J);
+
+	// b. define fiber stress
+	//-------------------------
+	if (l > 1.0)
+	{
+		double Wl = 0.0;
+		if (l < m_flam)
+		{
+			Wl = m_c3*(exp(m_c4*(l - 1.0)) - 1.0);
+		}
+		else
+		{
+			double c6 = m_c3*(exp(m_c4*(m_flam - 1.0)) - 1.0) - m_c5*m_flam;
+			Wl = m_c5*l + c6;
+		}
+		s += A*(Wl/J);
+	}
+
+	// c. define dilational stress
+	//------------------------------
+	// U(J) = 1/2*k*(lnJ)^2
+	double UJ = m_K*log(J)/J;
+	s += I*UJ;
+
+	return s;
+}
+
+//-----------------------------------------------------------------------------
+//! Calculate the spatial elasticity tangent
+tens4ds FECoupledTransIsoVerondaWestmann::Tangent(FEMaterialPoint& mp)
+{
+	// get material point data
+	FEElasticMaterialPoint& pt = *mp.ExtractData<FEElasticMaterialPoint>();
+
+	// calculate left Cauchy-Green tensor: B = F*Ft
+	mat3ds B = pt.LeftCauchyGreen();
+
+	// calculate square of B
+	mat3ds B2 = B*B;
+
+	// get the material fiber axis
+	vec3d a0;
+	a0.x = pt.m_Q[0][0];
+	a0.y = pt.m_Q[1][0];
+	a0.z = pt.m_Q[2][0];
+
+	// get the spatial fiber axis
+	vec3d a = pt.m_F*a0;
+	double l = a.unit();
+
+	// Invariants of B (= invariants of C)
+	double I1 = B.tr();
+	double J = pt.m_J;
+	double Ji = 1.0/J;
+	double I4 = l*l;
+
+	// some useful tensors
+	mat3dd I(1.0);
+	mat3ds A = dyad(a);
+	tens4ds IxI = dyad1s(I);
+	tens4ds IoI = dyad4s(I);
+	tens4ds BxB = dyad1s(B);
+	tens4ds BoB = dyad4s(B);
+	tens4ds AxA = dyad1s(A);
+
+	// a. matrix tangent
+	//----------------------------------
+	double W1 = m_c1*m_c2*exp(m_c2*(I1-3));
+	double W11 = m_c2*W1;
+	double W2 = -0.5*m_c1*m_c2;
+	tens4ds c = BxB*(4.0*(W11+W2)/J) - BoB*(4.0*W2/J);
+
+	// b. fiber tangent
+	// ---------------------------------
+	if (l > 1.0)
+	{
+		double Fl = 0.0, Fll = 0.0;
+		if (l < m_flam)
+		{
+			Fl = m_c3*(exp(m_c4*(l - 1.0)) - 1.0)/l;
+			Fll = -m_c3*(exp(m_c4*(l-1.0)) - 1.0)/(l*l) + m_c3*m_c4*exp(m_c4*(l-1.0))/l;
+		}
+		else
+		{
+			double c6 = m_c3*(exp(m_c4*(m_flam - 1.0)) - 1.0) - m_c5*m_flam;
+			Fl = m_c5 + c6 / l;
+			Fll = -c6/(l*l);
+		}
+
+		double W44 = (Fll - Fl/l)/(4*l*l);
+
+		c += AxA*(4.0*W44*I4*I4/J);
+	}
+
+	// c. dilational tangent
+	// ---------------------------------
+	double UJ = m_K*log(J)/J;
+	double UJJ = m_K*(1 - log(J))/(J*J);
+	c += IxI*(UJ + J*UJJ) - IoI*(2.0*UJ);
+
+	return c;
+}
