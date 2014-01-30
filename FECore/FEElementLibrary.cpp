1eb14f3ddb3e3ae7aef2d7ed3324aada10d750f9
SteveMaas1978 <steve.maas@utah.edu>
2014-01-30 05:48:12 +0000

    FEBio2: fixed some bugs in new tet15 implementation.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@5559 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FECore/FEElementLibrary.cpp
+++ FECore/FEElementLibrary.cpp
@@ -29,7 +29,7 @@ FEElementLibrary* FEElementLibrary::GetInstance()
 		n = m_pThis->RegisterTraits(new FETet10G4  ); assert(n==FE_TET10G4  );
 		n = m_pThis->RegisterTraits(new FETet10G8  ); assert(n==FE_TET10G8  );
 		n = m_pThis->RegisterTraits(new FETet10GL11); assert(n==FE_TET10GL11);
-		n = m_pThis->RegisterTraits(new FETet15G8  ); assert(n==FE_TET10G8  );
+		n = m_pThis->RegisterTraits(new FETet15G8  ); assert(n==FE_TET15G8  );
 		n = m_pThis->RegisterTraits(new FEHex20G27 ); assert(n==FE_HEX20G27 );
 		n = m_pThis->RegisterTraits(new FEQuad4G4  ); assert(n==FE_QUAD4G4  );
 		n = m_pThis->RegisterTraits(new FEQuad4NI  ); assert(n==FE_QUAD4NI  );
