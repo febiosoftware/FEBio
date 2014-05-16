125c3e38b161ceb52a998bac24c4a5d04376e4f5
rawlins <user@email>
2014-05-16 20:31:05 +0000

    FEBio2: Update build system
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@5924 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- build/Makelibs.mk
+++ build/Makelibs.mk
@@ -1,17 +1,18 @@
 include ../../make.$(PLAT)
 
 LIBDIR = $(notdir $(CURDIR))
+FELIB = $(shell echo $(LIBDIR) | tr A-Z a-z)
 
-SRC = $(wildcard $(FEBDIR)/$(LIBDIR)/*.cpp)
-OBJ = $(patsubst $(FEBDIR)/$(LIBDIR)/%.cpp, %.o, $(SRC))
-DEP = $(patsubst $(FEBDIR)/$(LIBDIR)/%.cpp, %.d, $(SRC))
+SRC = $(wildcard $(FEBDIR)$(LIBDIR)/*.cpp)
+OBJ = $(patsubst $(FEBDIR)$(LIBDIR)/%.cpp, %.o, $(SRC))
+DEP = $(patsubst $(FEBDIR)$(LIBDIR)/%.cpp, %.d, $(SRC))
 
-LIB = $(FEBDIR)/lib/lib$(FELIB)_$(PLAT).a
+LIB = $(FEBDIR)lib/lib$(FELIB)_$(PLAT).a
 
 $(LIB): $(OBJ)
 	ar -cvr $(LIB) $(OBJ)
 
-%.o: $(FEBDIR)/$(LIBDIR)/%.cpp
+%.o: $(FEBDIR)$(LIBDIR)/%.cpp
 	$(CC) -MMD -c $(INC) $(DEF) $(FLG) $<
 
 clean:
--- build/febio2.mk
+++ build/febio2.mk
@@ -1,20 +1,20 @@
-include $(FEBDIR)/make.$(PLAT)
+include ../../make.$(PLAT)
 
-SRC = $(wildcard $(FEBDIR)/FEBio2/*.cpp)
-OBJ = $(patsubst $(FEBDIR)/FEBio2/%.cpp, %.o, $(SRC))
-DEP = $(patsubst $(FEBDIR)/FEBio2/%.cpp, %.d, $(SRC))
+SRC = $(wildcard $(FEBDIR)FEBio2/*.cpp)
+OBJ = $(patsubst $(FEBDIR)FEBio2/%.cpp, %.o, $(SRC))
+DEP = $(patsubst $(FEBDIR)FEBio2/%.cpp, %.d, $(SRC))
 
-TARGET =  $(FEBDIR)/bin/febio2.$(PLAT)
+TARGET =  $(FEBDIR)bin/febio2.$(PLAT)
 
-FELIBS =  $(FEBDIR)/lib/libfecore_$(PLAT).a
-FELIBS += $(FEBDIR)/lib/libfebiolib_$(PLAT).a
-FELIBS += $(FEBDIR)/lib/libfebioplot_$(PLAT).a
-FELIBS += $(FEBDIR)/lib/libfebiomech_$(PLAT).a
-FELIBS += $(FEBDIR)/lib/libfebiomix_$(PLAT).a
-FELIBS += $(FEBDIR)/lib/libfebioheat_$(PLAT).a
-FELIBS += $(FEBDIR)/lib/libfebioxml_$(PLAT).a
-FELIBS += $(FEBDIR)/lib/libnumcore_$(PLAT).a
-FELIBS += $(FEBDIR)/lib/libfebioopt_$(PLAT).a
+FELIBS =  $(FEBDIR)lib/libfecore_$(PLAT).a
+FELIBS += $(FEBDIR)lib/libfebiolib_$(PLAT).a
+FELIBS += $(FEBDIR)lib/libfebioplot_$(PLAT).a
+FELIBS += $(FEBDIR)lib/libfebiomech_$(PLAT).a
+FELIBS += $(FEBDIR)lib/libfebiomix_$(PLAT).a
+FELIBS += $(FEBDIR)lib/libfebioheat_$(PLAT).a
+FELIBS += $(FEBDIR)lib/libfebioxml_$(PLAT).a
+FELIBS += $(FEBDIR)lib/libnumcore_$(PLAT).a
+FELIBS += $(FEBDIR)lib/libfebioopt_$(PLAT).a
 
 FEBIOLIBS = -Wl,--start-group $(FELIBS) -Wl,--end-group
 
@@ -23,11 +23,13 @@ ifeq ($(findstring lnx,$(PLAT)),lnx)
 	$(CC) -o $(TARGET) $(DEF) $(FLG) $(INC) $(OBJ) $(FEBIOLIBS) $(LIBS) -ldl
 else ifeq ($(findstring gcc,$(PLAT)),gcc)
 	$(CC) -o $(TARGET) $(DEF) $(FLG) $(INC) $(OBJ) $(FEBIOLIBS) $(LIBS) -ldl
+else ifeq ($(findstring sky,$(PLAT)),sky)
+	$(CC) -o $(TARGET) $(DEF) $(FLG) $(INC) $(OBJ) $(FEBIOLIBS) $(LIBS) -ldl
 else
 	$(CC) -o $(TARGET) $(DEF) $(FLG) $(INC) $(OBJ) $(FELIBS) $(LIBS)
 endif
 
-%.o: $(FEBDIR)/FEBio2/%.cpp
+%.o: $(FEBDIR)FEBio2/%.cpp
 	$(CC) $(INC) $(DEF) $(FLG) -MMD -c $<
 
 clean:
