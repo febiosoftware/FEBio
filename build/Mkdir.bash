519d9ffef26ddf87eedc61866c5d9bd72ec17532
rawlins <user@email>
2014-05-16 16:43:41 +0000

    FEBio2: Updated build directory
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@5920 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- build/Mkdir.bash
+++ build/Mkdir.bash
@@ -5,33 +5,17 @@ if [ $# == 0 ]; then
 	exit
 fi
 
-cd FEBio2
 mkdir $1
-cd ..
-cd FEBioHeat
-mkdir $1
-cd ..
-cd FEBioLib
-mkdir $1
-cd ..
-cd FEBioMech
-mkdir $1
-cd ..
-cd FEBioMix
-mkdir $1
-cd ..
-cd FEBioOpt
-mkdir $1
-cd ..
-cd FEBioPlot
-mkdir $1
-cd ..
-cd FEBioXML
-mkdir $1
-cd ..
-cd FECore
-mkdir $1
-cd ..
-cd NumCore
-mkdir $1
-cd ..
+cd $1
+mkdir FEBio2
+mkdir FEBioHeat
+mkdir FEBioLib
+mkdir FEBioMech
+mkdir FEBioMix
+mkdir FEBioOpt
+mkdir FEBioPlot
+mkdir FEBioXML
+mkdir FECore
+mkdir NumCore
+
+
