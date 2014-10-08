88a828e5a00c19a09543524331a175ba8c863cf8
rawlins <user@email>
2014-10-08 15:59:18 +0000

    FEBio2: Added GSL library to Linux64
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@6354 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- build/lnx64d.mk
+++ build/lnx64d.mk
@@ -3,11 +3,11 @@
 CC = icpc
 
 # Remove -DHAVE_LEVMAR and $(LEV_LIB) from LIBS if not linking with the Lourakis levmar routine.
-DEF = -DLINUX -DPARDISO -DHAVE_LEVMAR -DHAVE_ZLIB
+DEF = -DLINUX -DPARDISO -DHAVE_LEVMAR -DHAVE_ZLIB -DHAVE_GSL
 
 FLG = -O3 -fPIC -openmp -static-intel -no-intel-extensions
 
-#Intel Compiler
+# Intel Compiler
 INTELROOT = $(subst /mkl,,$(MKLROOT))/compiler
 INTEL_INC = $(INTELROOT)/include
 INTEL_LIB = $(INTELROOT)/lib/intel64
@@ -18,9 +18,12 @@ MKL_LIB = -Wl,--start-group $(MKL_PATH)/libmkl_intel_lp64.a
 MKL_LIB += $(MKL_PATH)/libmkl_intel_thread.a $(MKL_PATH)/libmkl_core.a -Wl,--end-group
 MKL_LIB += $(INTEL_LIB)/libiomp5.a -pthread -lz
 
-#Levmar library
+# Levmar library
 LEV_LIB = -llevmar_$(PLAT)
 
-LIBS = -L$(FEBDIR)/build/lib $(LEV_LIB) $(MKL_LIB)
+# GSL library
+GSL_LIB = -lgsl_$(PLAT)
+
+LIBS = -L$(FEBDIR)/build/lib $(LEV_LIB) $(GSL_LIB) $(MKL_LIB)
 
 INC = -I$(INTEL_INC) -I$(FEBDIR) -I$(FEBDIR)build/include
