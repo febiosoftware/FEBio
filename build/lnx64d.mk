0e866de8fc0ad579719b01bf56d44ce8f3eca0bc
rawlins <user@email>
2014-11-19 17:14:15 +0000

    FEBio2: Added SuperLU to the Linux build
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@6432 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- build/lnx64d.mk
+++ build/lnx64d.mk
@@ -3,7 +3,7 @@
 CC = icpc
 
 # Remove -DHAVE_LEVMAR and $(LEV_LIB) from LIBS if not linking with the Lourakis levmar routine.
-DEF = -DLINUX -DPARDISO -DHAVE_LEVMAR -DHAVE_ZLIB -DHAVE_GSL
+DEF = -DLINUX -DPARDISO -DSUPERLU -DHAVE_LEVMAR -DHAVE_ZLIB -DHAVE_GSL
 
 FLG = -O3 -fPIC -openmp -static-intel -no-intel-extensions
 
@@ -15,7 +15,7 @@ INTEL_LIB = $(INTELROOT)/lib/intel64
 # Pardiso solver
 MKL_PATH = $(MKLROOT)/lib/intel64
 MKL_LIB = -Wl,--start-group $(MKL_PATH)/libmkl_intel_lp64.a
-MKL_LIB += $(MKL_PATH)/libmkl_intel_thread.a $(MKL_PATH)/libmkl_core.a -Wl,--end-group
+MKL_LIB += $(MKL_PATH)/libmkl_core.a $(MKL_PATH)/libmkl_intel_thread.a -Wl,--end-group
 MKL_LIB += $(INTEL_LIB)/libiomp5.a -pthread -lz
 
 # Levmar library
@@ -24,6 +24,14 @@ LEV_LIB = -llevmar_$(PLAT)
 # GSL library
 GSL_LIB = -lgsl_$(PLAT)
 
-LIBS = -L$(FEBDIR)/build/lib $(LEV_LIB) $(GSL_LIB) $(MKL_LIB)
+# SuperLU library
+SUPERLU_INC = /home/sci/rawlins/Projects/SuperLU/4.3/SRC/
+SUPERLU_LIB =	-lsuperlu_4.3
 
-INC = -I$(INTEL_INC) -I$(FEBDIR) -I$(FEBDIR)build/include
+# SuperLU_MT library
+SUPERLUMT_INC = /home/sci/rawlins/Projects/SuperLU_MT_2.2/SRC/
+SUPERLUMT_LIB =	-lsuperlu_mt_OPENMP
+
+LIBS = -L$(FEBDIR)/build/lib $(LEV_LIB) $(SUPERLU_LIB) $(MKL_LIB) $(GSL_LIB)
+
+INC = -I$(INTEL_INC) -I$(FEBDIR) -I$(FEBDIR)build/include -I$(SUPERLU_INC)
--- build/lnx64g.mk
+++ build/lnx64g.mk
@@ -1,6 +1,6 @@
 # Make include file for FEBio on Linux
 
-include $(FEBDIR)build/lnx64d.mk
+include $(FEBDIR)build/lnx64.mk
 
 FLG := $(FLG:O3=g) # Note that we had to use := so that FLG is not recursive
 
