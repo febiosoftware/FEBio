ca53f2183fbf05d5b03fe446da9b8a735d75b0ff
SteveMaas1978 <steve.maas@utah.edu>
2014-12-30 00:24:15 +0000

    FEBio2: Added support for new tet15 integration rules in SPR stress projection.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@6519 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEBioMech/FESPRProjection.cpp
+++ FEBioMech/FESPRProjection.cpp
@@ -34,6 +34,8 @@ void FESPRProjection::Project(FESolidDomain& dom, const vector< vector<double> >
 	case FE_TET10G8  : { NDOF = 10; NCN = 4; } break;
 	case FE_TET10GL11: { NDOF = 10; NCN = 4; } break;
 	case FE_TET15G8  : { NDOF = 10; NCN = 4; } break;
+	case FE_TET15G11 : { NDOF = 10; NCN = 4; } break;
+	case FE_TET15G15 : { NDOF = 10; NCN = 4; } break;
 	case FE_HEX8G8   : { NDOF =  7; NCN = 8; } break;
 	default:
 		return;
