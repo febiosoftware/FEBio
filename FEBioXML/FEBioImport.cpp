47d05598a7d51e5faa17dd78692723e9450d153c
SteveMaas1978 <steve.maas@utah.edu>
2015-01-28 16:49:30 +0000

    FEBio2: changed default integration rule for tet15 to G15.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@6621 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEBioXML/FEBioImport.cpp
+++ FEBioXML/FEBioImport.cpp
@@ -251,7 +251,7 @@ bool FEFEBioImport::Load(FEModel& fem, const char* szfile)
 	m_ntet4  = FE_TET4G1;
 	m_nhex8  = FE_HEX8G8;
 	m_ntet10 = FE_TET10G4;
-	m_ntet15 = FE_TET15G8;
+	m_ntet15 = FE_TET15G15;
 	m_ntri6  = FE_TRI6G3;
 	m_ntri3  = FE_TRI3G3;
 	m_ntri7  = FE_TRI7G7;
