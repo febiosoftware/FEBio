882d840c51da0fb20468f10c1412f4afcd4adda0
gateshian <ateshian@columbia.edu>
2015-07-06 23:09:41 +0000

    febio2: updated FEDonnanEquilibrium to allow specification of properties in a manner compatible with growth analyses.  Backward compatibility is maintained.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@7228 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEBioMech/FEDonnanEquilibrium.cpp
+++ FEBioMech/FEDonnanEquilibrium.cpp
@@ -13,6 +13,7 @@
 // define the material parameters
 BEGIN_PARAMETER_LIST(FEDonnanEquilibrium, FEElasticMaterial)
 	ADD_PARAMETER(m_phiwr, FE_PARAM_DOUBLE, "phiw0");
+    ADD_PARAMETER(m_phisr, FE_PARAM_DOUBLE, "phis0");
 	ADD_PARAMETER(m_cFr, FE_PARAM_DOUBLE, "cF0");
 	ADD_PARAMETER(m_bosm, FE_PARAM_DOUBLE, "bosm");
     ADD_PARAMETER(m_Phi, FE_PARAM_DOUBLE, "Phi");
@@ -22,9 +23,16 @@ END_PARAMETER_LIST();
 // FEDonnanEquilibrium
 void FEDonnanEquilibrium::Init()
 {
+    if (!m_binit) {
+        if (m_phisr >= 0) {
+            m_bnew = true;
+            m_phiwr = 1 - m_phisr;  // use value at t=0 to initialize
+        }
+        m_binit = true;
+    }
 	if (m_phiwr <= 0 || m_phiwr > 1) throw MaterialError("phiw0 must be between 0. and 1.");
 	if (m_bosm < 0) throw MaterialError("bosm must be positive.");
-    if (m_Phi < 0 || m_Phi > 1) throw MaterialError("Phi must be between 0. and 1.");
+    if (m_Phi < 0) throw MaterialError("Phi must be positive.");
 
 	m_Rgas = GetFEModel()->GetGlobalConstant("R");
 	m_Tabs = GetFEModel()->GetGlobalConstant("T");
@@ -43,7 +51,11 @@ mat3ds FEDonnanEquilibrium::Stress(FEMaterialPoint& mp)
 	double J = pt.m_J;
 	
 	// calculate fixed charge density in current configuration
-	double cF = m_phiwr*m_cFr/(J-1+m_phiwr);
+    double cF;
+    if (m_bnew)
+        cF = m_phiwr*m_cFr/(J-m_phisr);
+    else
+        cF = m_phiwr*m_cFr/(J-1+m_phiwr);
 	
 	// calculate osmotic pressure
 	double p = m_Rgas*m_Tabs*m_Phi*(sqrt(cF*cF+m_bosm*m_bosm) - m_bosm);
@@ -63,14 +75,22 @@ tens4ds FEDonnanEquilibrium::Tangent(FEMaterialPoint& mp)
 	double J = pt.m_J;
 
 	// calculate fixed charge density in current configuration
-	double cF = m_phiwr*m_cFr/(J-1+m_phiwr);
+    double cF;
+    if (m_bnew)
+        cF = m_phiwr*m_cFr/(J-m_phisr);
+    else
+        cF = m_phiwr*m_cFr/(J-1+m_phiwr);
 	
 	// calculate osmotic pressure
 	double tosm = sqrt(cF*cF+m_bosm*m_bosm);	// tissue osmolarity
 	double p = m_Rgas*m_Tabs*m_Phi*(tosm - m_bosm);	// osmotic pressure
 	
 	// calculate derivative of osmotic pressure w.r.t. J
-	double bpi = m_Rgas*m_Tabs*m_Phi*J*cF*cF/(J-1+m_phiwr)/tosm;
+    double bpi;
+    if (m_bnew)
+        bpi = m_Rgas*m_Tabs*m_Phi*J*cF*cF/(J-m_phisr)/tosm;
+    else
+        bpi = m_Rgas*m_Tabs*m_Phi*J*cF*cF/(J-1+m_phiwr)/tosm;
 	
 	mat3dd I(1.0);	// Identity
 	
--- FEBioMech/FEDonnanEquilibrium.h
+++ FEBioMech/FEDonnanEquilibrium.h
@@ -16,7 +16,9 @@ class FEDonnanEquilibrium : public FEElasticMaterial
 public:
 	//! When used on its own (not in a solid mixture), this materials
 	//! is intrinsically unstable
-    FEDonnanEquilibrium(FEModel* pfem) : FEElasticMaterial(pfem) { m_Rgas = 0; m_Tabs = 0; m_cFr = 0; m_phiwr = 0; m_Phi = 1;}
+    FEDonnanEquilibrium(FEModel* pfem) : FEElasticMaterial(pfem) {
+        m_Rgas = 0; m_Tabs = 0; m_cFr = 0; m_phiwr = -1; m_phisr = -1;
+        m_bnew = false; m_binit = false; m_Phi = 1;}
 	
 	//! Initialization routine
 	void Init();
@@ -32,9 +34,12 @@ public:
 	
 public:
 	double	m_phiwr;	//!< fluid volume fraction in reference configuration
+    double	m_phisr;	//!< referential solid volume fraction (may evolve with time)
 	double	m_cFr;		//!< fixed charge density in reference configuration
 	double	m_Rgas;		//!< universal gas constant
 	double	m_Tabs;		//!< absolute temperature
 	double	m_bosm;		//!< bath osmolarity
     double  m_Phi;      //!< osmotic coefficient
+    bool    m_bnew;     //!< flag for using old or new method
+    bool    m_binit;    //!< initialization flag
 };
