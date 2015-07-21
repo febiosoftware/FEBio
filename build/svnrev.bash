079a70dc85596985b71dce851a8134da5e124ab5
rawlins <user@email>
2015-07-21 16:49:16 +0000

    FEBio2: Added bash script to create svn revision number for Linux and Mac
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@7279 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ build/svnrev.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Executing this bash script before compiling FEBio will add the svn revision number to the version
+# number in the FEBio splash screen.  Note that this will only work if FEBio is under svn version control.
+
+temp=$(svnversion -n)
+svnrev=$( echo $temp | cut -f1 -d'M')
+
+sed s/[$]WCREV[$]/$svnrev/ FEBioLib/svnrev_tpl.txt > FEBio/svnrev.h
