e86d20722701b0578cf25c9d04fa61dc79fef68b
rawlins <user@email>
2015-10-28 23:10:10 +0000

    FEBio2: Modification for compiling on Linux and Mac
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@7690 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEBioMech/FEMortarSlidingContact.cpp
+++ FEBioMech/FEMortarSlidingContact.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "FEMortarSlidingContact.h"
 #include "FEStiffnessMatrix.h"
-#include <FECore\FEModel.h>
+#include <FECore/FEModel.h>
 
 //=============================================================================
 // FEMortarSurface
