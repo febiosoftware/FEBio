054d3392a01d9b23fb73f22239c178830f3a5090
SteveMaas1978 <steve.maas@utah.edu>
2016-05-26 14:23:08 +0000

    FEBio2: forgot to commit files.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@8413 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FECore/FEDataArray.cpp
@@ -0,0 +1,137 @@
+#include "stdafx.h"
+#include "FEDataArray.h"
+#include "DumpStream.h"
+
+//-----------------------------------------------------------------------------
+template <> bool FEDataArray::set<double>(int n, const double& v)
+{
+	assert(m_dataType == FE_DOUBLE);
+	m_val[n] = v;
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+template <> bool FEDataArray::set<vec2d>(int n, const vec2d& v)
+{
+	assert(m_dataType == FE_VEC2D);
+	m_val[2*n  ] = v.x();
+	m_val[2*n+1] = v.y();
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+template <> bool FEDataArray::set<vec3d>(int n, const vec3d& v)
+{
+	assert(m_dataType == FE_VEC3D);
+	m_val[3*n  ] = v.x;
+	m_val[3*n+1] = v.y;
+	m_val[3*n+2] = v.z;
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+template <> bool FEDataArray::set<double>(const double& v)
+{
+	assert(m_dataType == FE_DOUBLE);
+	m_defDouble = v;
+	for (int i=0; i<(int) m_val.size(); ++i) m_val[i] = v;
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+template <> bool FEDataArray::set<vec2d>(const vec2d& v)
+{
+	assert(m_dataType == FE_VEC2D);
+	m_defVec2d = v;
+	for (int i=0; i<(int) m_val.size(); i += 2)
+	{
+		m_val[i  ] = v.x();
+		m_val[i+1] = v.y();
+	}
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+template <> bool FEDataArray::set<vec3d>(const vec3d& v)
+{
+	assert(m_dataType == FE_VEC3D);
+	m_defVec3d = v;
+	for (int i=0; i<(int) m_val.size(); i += 3)
+	{
+		m_val[i  ] = v.x;
+		m_val[i+1] = v.y;
+		m_val[i+2] = v.z;
+	}
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+FEDataArray::FEDataArray(int dataType) : m_dataType(dataType)
+{
+	m_defDouble = 0.0;
+	m_defVec3d = vec3d(0,0,0);
+}
+
+//-----------------------------------------------------------------------------
+FEDataArray::FEDataArray(const FEDataArray& map)
+{
+	m_dataType = map.m_dataType;
+	m_val = map.m_val;
+}
+
+//-----------------------------------------------------------------------------
+FEDataArray& FEDataArray::operator = (const FEDataArray& map)
+{
+	m_dataType = map.m_dataType;
+	m_val = map.m_val;
+	return *this;
+}
+
+//-----------------------------------------------------------------------------
+int FEDataArray::DataSize() const
+{
+	switch (m_dataType)
+	{
+	case FE_DOUBLE : return 1; break;
+	case FE_VEC2D  : return 2; break;
+	case FE_VEC3D  : return 3; break;
+	}
+	assert(false);
+	return 0;
+}
+
+//-----------------------------------------------------------------------------
+int FEDataArray::size() const
+{
+	return (int)m_val.size() / DataSize();
+}
+
+//-----------------------------------------------------------------------------
+bool FEDataArray::resize(int n)
+{
+	switch (m_dataType)
+	{
+	case FE_DOUBLE: m_val.resize(n*DataSize(), m_defDouble); break;
+	case FE_VEC2D : m_val.resize(n*DataSize()); set(m_defVec2d); break;
+	case FE_VEC3D : m_val.resize(n*DataSize()); set(m_defVec3d); break;
+	default:
+		assert(false);
+		return false;
+	}
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+void FEDataArray::Serialize(DumpStream& ar)
+{
+	if (ar.IsSaving())
+	{
+		ar << m_dataType;
+		ar << m_val;
+	}
+	else
+	{
+		ar >> m_dataType;
+		ar >> m_val;
+	}
+}
---
+++ FECore/FEDataArray.h
@@ -0,0 +1,109 @@
+#pragma once
+#include <vector>
+#include <assert.h>
+#include "vec3d.h"
+#include "vec2d.h"
+
+//-----------------------------------------------------------------------------
+class DumpStream;
+
+//-----------------------------------------------------------------------------
+enum FEDataType {
+	FE_DOUBLE,
+	FE_VEC2D,
+	FE_VEC3D
+};
+
+//-----------------------------------------------------------------------------
+class FEDataArray
+{
+public:
+	//! default constructor
+	FEDataArray(int dataType);
+
+	//! copy constructor
+	FEDataArray(const FEDataArray& map);
+
+	//! assignment operator
+	FEDataArray& operator = (const FEDataArray& map);
+
+	//! allocate data
+	bool resize(int nsize);
+
+	//! return the size of array (units of data type)
+	int size() const;
+
+	//! get the value for a given facet index
+	template <class T> T get(int n) const;
+
+	//! set the value
+	template <class T> bool set(const T& v);
+
+	//! set the value
+	template <class T> bool set(int n, const T& v);
+
+	//! add a value
+	template <class T> void push_back(const T& v);
+
+public:
+	//! get data type
+	int DataType() const { return m_dataType; }
+
+	//! return the buffer size (actual number of doubles)
+	int BufferSize() const { return (int) m_val.size(); }
+
+	//! serialization
+	void Serialize(DumpStream& ar);
+
+protected:
+	int DataSize() const;
+
+private:
+	int	m_dataType;				//!< data type
+
+	// default values
+	double	m_defDouble;
+	vec3d	m_defVec3d;
+	vec2d	m_defVec2d;
+
+	std::vector<double>	m_val;	//!< data values
+};
+
+template <> inline double FEDataArray::get<double>(int n) const
+{
+	assert(m_dataType == FE_DOUBLE);
+	return	m_val[n];
+}
+
+template <> inline vec2d FEDataArray::get<vec2d>(int n) const
+{
+	assert(m_dataType == FE_VEC2D);
+	return	vec2d(m_val[2*n], m_val[2*n+1]);
+}
+
+template <> inline vec3d FEDataArray::get<vec3d>(int n) const
+{
+	assert(m_dataType == FE_VEC3D);
+	return	vec3d(m_val[3*n], m_val[3*n + 1], m_val[3*n+2]);
+}
+
+template <> inline void FEDataArray::push_back<double>(const double& v)
+{
+	assert(m_dataType == FE_DOUBLE);
+	m_val.push_back(v);
+}
+
+template <> inline void FEDataArray::push_back<vec2d>(const vec2d& v)
+{
+	assert(m_dataType == FE_VEC2D);
+	m_val.push_back(v.x());
+	m_val.push_back(v.y());
+}
+
+template <> inline void FEDataArray::push_back<vec3d>(const vec3d& v)
+{
+	assert(m_dataType == FE_VEC3D);
+	m_val.push_back(v.x);
+	m_val.push_back(v.y);
+	m_val.push_back(v.z);
+}
