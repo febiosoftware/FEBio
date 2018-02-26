76027ab0571710ca512ac3aedc15d7ab70f64e69
gateshian <ateshian@columbia.edu>
2018-02-26 01:09:14 +0000

    updated llvm.mk
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@10448 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- build/llvm.mk
+++ build/llvm.mk
@@ -6,7 +6,8 @@ LLVM_PATH = /usr/local/opt/llvm
 CC = $(LLVM_PATH)/bin/clang++
 
 # Remove -DHAVE_LEVMAR and $(LEV_LIB) from LIBS if not linking with the Lourakis levmar routine.
-DEF = -DPARDISO -DHAVE_LEVMAR -DSVN
+# Remove -DHAVE_GSL and $(GSL_LIB) from LIBS if not linking with the GNU scientific library.
+DEF = -DPARDISO -DMKL_ISS -DHAVE_LEVMAR -DHAVE_GSL -DSVN
 
 FLG = -O3 -fopenmp -fPIC -std=c++11 -stdlib=libstdc++
 
@@ -17,6 +18,9 @@ MKL_LIB = $(MKL_PATH)libmkl_intel_lp64.a $(MKL_PATH)libmkl_intel_thread.a $(MKL_
 #Levmar library
 LEV_LIB = -llevmar
 
-LIBS = -L$(LLVM_PATH)/lib -L$(FEBDIR)build/lib $(LEV_LIB) $(MKL_LIB)
+#GSL library
+GSL_LIB = -lgsl
+
+LIBS = -L$(LLVM_PATH)/lib -L$(FEBDIR)build/lib $(LEV_LIB) $(GSL_LIB) $(MKL_LIB)
 
 INC = -I$(LLVM_PATH)/include -I$(FEBDIR) -I$(FEBDIR)build/include
