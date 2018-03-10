ba8f3d7a7b1eb817b5e4f90fa7410c4fa2ac445a
SteveMaas1978 <steve.maas@utah.edu>
2018-03-10 00:10:02 +0000

    FEBio2: Forgot to commit missing files.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@10522 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FECore/FEDataMathGenerator.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "FENodeDataMap.h"
+#include "FESurfaceMap.h"
+#include <string>
+
+class FENodeSet;
+class FEFacetSet;
+
+//-----------------------------------------------------------------------------
+class FEDataMathGenerator
+{
+public:
+	FEDataMathGenerator();
+
+	// set the math expression
+	void setExpression(const std::string& math);
+
+	// generate the data array for the given node set
+	bool Generate(FENodeDataMap& ar, const FENodeSet& set);
+
+	// generate the data array for the given facet set
+	bool Generate(FESurfaceMap& data, const FEFacetSet& surf);
+
+private:
+	std::string	m_math;
+};
---
+++ FECore/FENodeDataMap.cpp
@@ -0,0 +1,52 @@
+#include "stdafx.h"
+#include "FENodeDataMap.h"
+
+FENodeDataMap::FENodeDataMap(int dataType) : FEDataArray(dataType)
+{
+	
+}
+
+void FENodeDataMap::Create(int nsize, double val)
+{
+	resize(nsize, val);
+}
+
+void FENodeDataMap::Add(double val)
+{
+	push_back(val);
+}
+
+double FENodeDataMap::getValue(int n) const
+{
+	return get<double>(n);
+}
+
+void FENodeDataMap::setValue(int n, double v)
+{
+	set<double>(n, v);
+}
+
+void FENodeDataMap::setValue(int n, const vec2d& v)
+{
+	set<vec2d>(n, v);
+}
+
+void FENodeDataMap::setValue(int n, const vec3d& v)
+{
+	set<vec3d>(n, v);
+}
+
+void FENodeDataMap::fillValue(double v)
+{
+	set<double>(v);
+}
+
+void FENodeDataMap::fillValue(const vec2d& v)
+{
+	set<vec2d>(v);
+}
+
+void FENodeDataMap::fillValue(const vec3d& v)
+{
+	set<vec3d>(v);
+}
