bd593e9f2d89260ec6b1e10607d8a24485404330
gateshian <ateshian@columbia.edu>
2018-03-19 18:23:55 +0000

    updated osxd.mk makefile for Intel compiler on Mac
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@10590 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- build/osxd.mk
+++ build/osxd.mk
@@ -3,7 +3,7 @@
 CC = icpc
 
 # Remove -DHAVE_LEVMAR and $(LEV_LIB) from LIBS if not linking with the Lourakis levmar routine.
-DEF = -DPARDISO -DHAVE_LEVMAR -DHAVE_ZLIB -DSVN
+DEF = -DPARDISO -DMKL_ISS -DHAVE_LEVMAR -DHAVE_GSL -DSVN
 
 FLG = -O3 -fopenmp -fPIC -static-intel -no-intel-extensions -std=c++11
 
@@ -16,8 +16,10 @@ MKL_LIB = $(MKL_PATH)libmkl_intel_lp64.a $(MKL_PATH)libmkl_intel_thread.a $(MKL_
 	$(INTEL_LIB)libiomp5.a -pthread -lz
 
 #Levmar library
-LEV_LIB = -llevmar_$(PLAT)
+LEV_LIB = -llevmar
 
-LIBS = -L$(FEBDIR)build/lib $(LEV_LIB) $(MKL_LIB)
+GSL_LIB = -lgsl
+
+LIBS = -L$(FEBDIR)build/lib $(LEV_LIB) $(GSL_LIB) $(MKL_LIB)
 
 INC = -I$(INTEL_INC) -I$(FEBDIR) -I$(FEBDIR)build/include
