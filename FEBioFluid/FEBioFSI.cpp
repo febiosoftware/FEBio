ff4c1a578beac1c68145ba803201b225a33a9419
SteveMaas1978 <steve.maas@utah.edu>
2018-03-26 15:44:08 +0000

    FEBio2: forgot to commit files.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio2@10622 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FEBioFluid/FEBioFSI.cpp
@@ -0,0 +1,29 @@
+#include "FEBioFSI.h"
+#include <FECore/FECoreKernel.h>
+#include "FEFluidFSISolver.h"
+#include "FEFluidFSI.h"
+#include "FEFluidFSIDomain3D.h"
+#include "FEFluidFSITraction.h"
+#include "FEFluidFSIDomainFactory.h"
+
+void FEBioFSI::InitModule()
+{
+	FECoreKernel& febio = FECoreKernel::GetInstance();
+
+	// register domain
+	febio.RegisterDomain(new FEFluidFSIDomainFactory);
+
+	// define the fsi module
+	febio.CreateModule("fluid-FSI");
+	febio.SetModuleDependency("fluid");
+
+	REGISTER_FECORE_CLASS(FEFluidFSISolver, FESOLVER_ID, "fluid-FSI");
+
+	REGISTER_FECORE_CLASS(FEFluidFSI, FEMATERIAL_ID, "fluid-FSI");
+
+	REGISTER_FECORE_CLASS(FEFluidFSIDomain3D, FEDOMAIN_ID, "fluid-FSI-3D");
+
+	REGISTER_FECORE_CLASS(FEFluidFSITraction, FESURFACELOAD_ID, "fluid-FSI traction");
+
+	febio.SetActiveModule(0);
+}
---
+++ FEBioFluid/FEFluidFSIDomainFactory.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "FEFluidFSIDomainFactory.h"
+#include "FEFluidFSI.h"
+#include <FECore/FEDomain.h>
+
+//-----------------------------------------------------------------------------
+FEDomain* FEFluidFSIDomainFactory::CreateDomain(const FE_Element_Spec& spec, FEMesh* pm, FEMaterial* pmat)
+{
+	FEModel* pfem = pmat->GetFEModel();
+	FE_Element_Class eclass = spec.eclass;
+	FE_Element_Shape eshape = spec.eshape;
+	const char* sztype = 0;
+	if (dynamic_cast<FEFluidFSI*>(pmat))
+	{
+		// fluid elements
+		if (eclass == FE_ELEM_SOLID) sztype = "fluid-FSI-3D";
+		else return 0;
+	}
+
+	if (sztype)
+	{
+		FEDomain* pd = fecore_new<FEDomain>(FEDOMAIN_ID, sztype, pfem);
+		if (pd) pd->SetMaterial(pmat);
+		return pd;
+	}
+	else return 0;
+}
