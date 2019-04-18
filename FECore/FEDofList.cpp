c386d5eb15503b76cf20006a5565b4acc90e29df
Steve Maas <steve.maas@utah.edu>
2019-04-18 16:52:18 +0000

    FEBio3: committed missing files.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio3@12060 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FECore/FEDofList.cpp
@@ -0,0 +1,95 @@
+/*This file is part of the FEBio source code and is licensed under the MIT license
+listed below.
+
+See Copyright-FEBio.txt for details.
+
+Copyright (c) 2019 University of Utah, Columbia University, and others.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+#include "stdafx.h"
+#include "FEDofList.h"
+#include "FEModel.h"
+
+FEDofList::FEDofList(FEModel* fem)
+{
+	m_fem = fem;
+}
+
+FEDofList::FEDofList(const FEDofList& dofs)
+{
+	m_fem = dofs.m_fem;
+	m_dofList = dofs.m_dofList;
+}
+
+void FEDofList::operator = (const FEDofList& dofs)
+{
+	assert(m_fem == dofs.m_fem);
+	m_dofList = dofs.m_dofList;
+}
+
+void FEDofList::Clear()
+{
+	m_dofList.clear();
+}
+
+bool FEDofList::AddDof(const char* szdof)
+{
+	int dof = m_fem->GetDOFIndex(szdof);
+	if (dof == -1) return false;
+	m_dofList.push_back(dof);
+	return true;
+}
+
+bool FEDofList::AddDof(int ndof)
+{
+	if ((ndof < 0) || (ndof >= m_fem->GetDOFS().GetTotalDOFS())) return false;
+	m_dofList.push_back(ndof);
+	return true;
+}
+
+bool FEDofList::AddVariable(const char* szvar)
+{
+	DOFS& Dofs = m_fem->GetDOFS();
+	std::vector<int> dofList;
+	Dofs.GetDOFList(szvar, dofList);
+	if (dofList.empty()) return false;
+
+	m_dofList.insert(m_dofList.end(), dofList.begin(), dofList.end());
+	return true;
+}
+
+bool FEDofList::IsEmpty() const
+{
+	return m_dofList.empty();
+}
+
+int FEDofList::Size() const
+{
+	return (int)m_dofList.size();
+}
+
+int FEDofList::operator [] (int n) const
+{
+	return m_dofList[n];
+}
+
+void FEDofList::Serialize(DumpStream& ar)
+{
+	ar & m_dofList;
+}
---
+++ FECore/FEDofList.h
@@ -0,0 +1,73 @@
+/*This file is part of the FEBio source code and is licensed under the MIT license
+listed below.
+
+See Copyright-FEBio.txt for details.
+
+Copyright (c) 2019 University of Utah, Columbia University, and others.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+#pragma once
+#include "fecore_api.h"
+#include <vector>
+
+//-----------------------------------------------------------------------------
+class FEModel;
+class DumpStream;
+
+//-----------------------------------------------------------------------------
+// Convenience class for creating a list of degrees of freedom, without the 
+// need to go through the FEModel class;
+
+class FECORE_API FEDofList
+{
+public:
+	FEDofList(FEModel* fem);
+	FEDofList(const FEDofList& dofs);
+
+	// assignment operator
+	void operator = (const FEDofList& dofs);
+
+	// clear the list
+	void Clear();
+
+	// Add a degree of freedom
+	bool AddDof(const char* szdof);
+
+	// Add a degree of freeomd
+	bool AddDof(int ndof);
+
+	// Add all the dofs of a variable
+	bool AddVariable(const char* szvar);
+
+	// is the list empty
+	bool IsEmpty() const;
+
+	// number of dofs in the list
+	int Size() const;
+
+	// access a dof
+	int operator [] (int n) const;
+
+	// serialization
+	void Serialize(DumpStream& ar);
+
+private:
+	FEModel*			m_fem;
+	std::vector<int>	m_dofList;
+};
