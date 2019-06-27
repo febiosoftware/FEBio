3f0eb71fbc3f83e87d6ea5f068456405e3b6a76e
Steve Maas <steve.maas@utah.edu>
2019-06-27 16:22:42 +0000

    FEBio3: removed some code related to partitioning that is no longer needed.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio3@12301 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEBioFluid/FEFluidSolver.cpp
+++ FEBioFluid/FEFluidSolver.cpp
@@ -195,15 +195,6 @@ bool FEFluidSolver::InitEquations()
         if (n.m_ID[m_dofEF ] != -1) m_ndeq++;
     }
 
-	// TODO: I don't think this is necessarry anymore since this is calculated in FESolver::InitEquations
-	if (m_eq_scheme == EQUATION_SCHEME::BLOCK)
-	{
-		vector<int> part;
-		part.push_back(m_nveq);
-		part.push_back(m_ndeq);
-		SetPartitions(part);
-	}
-
     return true;
 }
 
