3720eceee1ee05bc79749b0e7aed9ac1c5293340
Gerard Ateshian <ateshian@columbia.edu>
2019-11-10 21:14:50 +0000

    FEBio3: updated llvm.mk
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio3@12840 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- build/llvm.mk
+++ build/llvm.mk
@@ -16,9 +16,11 @@ MKL_PATH = $(MKLROOT)/lib/
 MKL_LIB = $(MKL_PATH)libmkl_intel_lp64.a $(MKL_PATH)libmkl_intel_thread.a $(MKL_PATH)libmkl_core.a
 
 #Levmar library
+LEV_PATH = /usr/local
 LEV_LIB = -llevmar
 
 #GSL library
+GSL_PATH = /usr/local/opt/gsl
 GSL_LIB = -lgsl
 
 #HYPRE library
@@ -27,13 +29,16 @@ HYPRE_PATH = /usr/local/hypre-master/src/hypre
 
 #OMP library
 OMP_PATH = /usr/local/opt/libomp
+OMP_LIB = -lomp
 
 #MPI library
+MPI_PATH = /usr/local/opt/open-mpi
 MPI_LIB = -lmpi
 
 #MMG library
+MMG_PATH = /usr/local/mmg-master/build
 MMG_LIB = -lmmg3d
 
-LIBS = -L$(LLVM_PATH)/lib -L$(FEBDIR)build/lib -L$(HYPRE_PATH)/lib $(HYPRE_LIB) $(LEV_LIB) $(GSL_LIB) $(MKL_LIB) $(MPI_LIB) $(MMG_LIB)
+LIBS = -L$(LLVM_PATH)/lib -L$(FEBDIR)build/lib -L$(HYPRE_PATH)/lib $(HYPRE_LIB) -L$(LEV_PATH)/lib $(LEV_LIB) -L$(GSL_PATH)/lib $(GSL_LIB) $(MKL_LIB) -L$(OMP_PATH)/lib $(OMP_LIB) -L$(MPI_PATH)/lib $(MPI_LIB) -L$(MMG_PATH)/lib $(MMG_LIB)
 
-INC = -I$(LLVM_PATH)/include -I$(FEBDIR) -I$(FEBDIR)build/include -I$(HYPRE_PATH)/include -I$(OMP_LIB)/include
+INC = -I$(LLVM_PATH)/include -I$(FEBDIR) -I$(FEBDIR)build/include -I$(HYPRE_PATH)/include -I$(OMP_PATH)/include -I$(MPI_PATH)/include -I$(MMG_PATH)/include -I$(GSL_PATH)/include -I$(LEV_PATH)/include
