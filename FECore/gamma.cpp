cd9b7fcb2e0c9150522783990b87f137d7eb7bbb
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2021-04-09 13:35:02 -0600

    Updated VS2017 project. Had to define M_PI for windows compilation.

--- FECore/gamma.cpp
+++ FECore/gamma.cpp
@@ -28,6 +28,10 @@
 #include <limits>
 #include <math.h>
 
+#ifndef M_PI
+#define M_PI 3.141592653589793238462643
+#endif
+
 //---------------------------------------------------------------------------------
 // evaluate natural logarithm of gamma function
 double gammaln(double x)
