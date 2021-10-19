329e697b2abe124afdf14a2e69eabbf853df13ba
michaelrossherron <michaelrossherron@gmail.com>
2021-10-19 14:17:52 -0600

    Updated copyright info in source files.

--- FEBioMech/FEContinuousElasticDamage.h
+++ FEBioMech/FEContinuousElasticDamage.h
@@ -3,7 +3,7 @@ listed below.
 
 See Copyright-FEBio.txt for details.
 
-Copyright (c) 2020 University of Utah, The Trustees of Columbia University in
+Copyright (c) 2021 University of Utah, The Trustees of Columbia University in
 the City of New York, and others.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -28,7 +28,7 @@ SOFTWARE.*/
 #include "FEElasticFiberMaterial.h"
 
 // These classes implement the elastic damage framework from 
-// Balzani, Brinkhues, Holzapfel, Comput. Methods Appl. Mech. Engrg. 213–216 (2012) 139–151
+// Balzani, Brinkhues, Holzapfel, Comput. Methods Appl. Mech. Engrg. 213Â–216 (2012) 139Â–151
 
 class FEFiberDamagePoint;
 
