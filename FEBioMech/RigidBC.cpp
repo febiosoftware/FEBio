1342b930642820ffd13ceb403e2f4f8d4a65e1c6
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2021-12-16 13:22:52 -0700

    Added error check for FERigidBodyFixedBC.

--- FEBioMech/RigidBC.cpp
+++ FEBioMech/RigidBC.cpp
@@ -33,6 +33,7 @@ SOFTWARE.*/
 #include "FEMechModel.h"
 #include "FERigidMaterial.h"
 #include <FECore/DumpStream.h>
+#include <FECore/log.h>
 
 REGISTER_SUPER_CLASS(FERigidBC, FERIGIDBC_ID);
 
@@ -172,10 +173,15 @@ bool FERigidBodyFixedBC::Init()
 	if (pm == 0) return false;
 
 	// make sure we have a valid dof
+	// TODO: Can I test this automatically during validation?
 	for (int i = 0; i < m_dofs.size(); ++i)
 	{
 		int dof_i = m_dofs[i];
-		if ((dof_i < 0) || (dof_i >= 6)) return false;
+		if ((dof_i < 0) || (dof_i >= 6))
+		{
+			feLogError("Invalid value for dofs of fixed rigid constraint %s", GetName().c_str());
+			return false;
+		}
 	}
 
 	m_binit = true;
