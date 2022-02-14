f7d4abaf217461f7a5e18d91ab4e2f295e5e3e24
Gerard Ateshian <ateshian@columbia.edu>
2022-02-14 09:18:44 -0500

    Added new uncoupled Holmes-Mow isotropic elastic solid.

---
+++ FEBioMech/FEHolmesMowUC.h
@@ -0,0 +1,54 @@
+/*This file is part of the FEBio source code and is licensed under the MIT license
+ listed below.
+ 
+ See Copyright-FEBio.txt for details.
+ 
+ Copyright (c) 2022 University of Utah, The Trustees of Columbia University in
+ the City of New York, and others.
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.*/
+
+
+
+#pragma once
+#include "FEUncoupledMaterial.h"
+
+//-----------------------------------------------------------------------------
+class FEHolmesMowUC : public FEUncoupledMaterial
+{
+public:
+    FEHolmesMowUC(FEModel* pfem) : FEUncoupledMaterial(pfem) {}
+    
+public:
+    double    m_mu;   //!< shear modulus
+    double    m_b;    //!< Exponential stiffening coefficient
+    
+public:
+    //! calculate stress at material point
+    virtual mat3ds DevStress(FEMaterialPoint& pt) override;
+    
+    //! calculate tangent stiffness at material point
+    virtual tens4ds DevTangent(FEMaterialPoint& pt) override;
+    
+    //! calculate strain energy density at material point
+    virtual double DevStrainEnergyDensity(FEMaterialPoint& pt) override;
+    
+    // declare the parameter list
+    DECLARE_FECORE_CLASS();
+};
