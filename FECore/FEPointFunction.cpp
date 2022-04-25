9ae8c99e585f81724ad3fb93d189ed82e63bd90a
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2022-04-25 14:51:57 -0600

    Fixed incorrect enum order for interpolate parameter of FEPointFunction.

--- FECore/FEPointFunction.cpp
+++ FECore/FEPointFunction.cpp
@@ -35,7 +35,7 @@ SOFTWARE.*/
 
 //-----------------------------------------------------------------------------
 BEGIN_FECORE_CLASS(FEPointFunction, FEFunction1D)
-	ADD_PARAMETER(m_int, "interpolate", FE_PARAM_ATTRIBUTE, "step\0linear\0smooth\0cubic spline\0control points\0approximation\0");
+	ADD_PARAMETER(m_int, "interpolate", FE_PARAM_ATTRIBUTE, "linear\0step\0smooth\0cubic spline\0control points\0approximation\0");
 	ADD_PARAMETER(m_ext, "extend"     , FE_PARAM_ATTRIBUTE, "constant\0extrapolate\0repeat\0repeat offset\0");
     ADD_PARAMETER(m_bln, "log");
 	ADD_PARAMETER(m_points, "points");
