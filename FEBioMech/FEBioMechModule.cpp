2fb783e72726108117763023aed742ac8fbf6b6f
michaelrossherron <michaelrossherron@gmail.com>
2022-04-26 16:56:33 -0600

    Aded FEBirzle material and documentation.

--- FEBioMech/FEBioMechModule.cpp
+++ FEBioMech/FEBioMechModule.cpp
@@ -156,6 +156,7 @@ SOFTWARE.*/
 #include "FEIsotropicLeeSacks.h"
 #include "FEIsotropicLeeSacksUncoupled.h"
 #include "FEPolynomialHyperElastic.h"
+#include "FEBirzle.h"
 
 #include "FEPressureLoad.h"
 #include "FETractionLoad.h"
@@ -319,6 +320,7 @@ void FEBioMech::InitModule()
 	REGISTER_FECORE_CLASS(FEDamageFiberPower, "damage fiber power");
 	REGISTER_FECORE_CLASS(FEDamageFiberExponential, "damage fiber exponential");
 	REGISTER_FECORE_CLASS(FEDamageFiberExpLinear, "damage fiber exp-linear");
+    REGISTER_FECORE_CLASS(FEBirzle, "birzle");
 
 	// These materials are derived from FEElasticMaterial and use FEElasticMaterials
 	REGISTER_FECORE_CLASS(FEElasticMixture, "solid mixture");
