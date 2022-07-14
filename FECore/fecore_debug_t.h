d3c32370d0bb7fbd7bd8a456191221f15738054c
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2022-07-14 10:06:46 -0600

    Added FECORE_API macro to FECoreDebugger.

--- FECore/fecore_debug_t.h
+++ FECore/fecore_debug_t.h
@@ -36,6 +36,7 @@ SOFTWARE.*/
 #include "mat3d.h"
 #include "vec3d.h"
 #include "tens4d.h"
+#include "fecore_api.h"
 
 // This file defines template constructions that are used by the FECore debugger.
 // Don't use anything in here or include this file directly. 
@@ -54,7 +55,7 @@ template <> void fecore_print_T<std::vector<int> >(std::vector<int>* pv);
 
 class FECoreBreakPoint;
 
-class FECoreDebugger
+class FECORE_API FECoreDebugger
 {
 public:
 	class Variable
