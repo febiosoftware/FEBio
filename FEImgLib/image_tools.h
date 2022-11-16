d336003a3cafa31c5226dedf1e0aaa41f941586b
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2022-11-16 12:17:01 -0700

    Implemented 2d and 3d fft algorithms using mkl. Implemented fft based 2d and 3d blurring.

--- FEImgLib/image_tools.h
+++ FEImgLib/image_tools.h
@@ -5,3 +5,6 @@ class Image;
 
 FEIMGLIB_API void blur_image_2d(Image& trg, Image& src, float d);
 FEIMGLIB_API void blur_image(Image& trg, Image& src, float d);
+
+FEIMGLIB_API void fftblur_2d(Image& trg, Image& src, float d);
+FEIMGLIB_API void fftblur_3d(Image& trg, Image& src, float d);
