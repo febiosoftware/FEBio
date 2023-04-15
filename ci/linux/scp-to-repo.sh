f7b73a02e44aea31af9f2b3b898b975dd3ba897a
John Haynes <tundranerd@gmail.com>
2023-04-15 16:21:06 -0600

    Reorganized Linux

---
+++ ci/linux/scp-to-repo.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+scp cmbuild/bin/* repo:~/update2/FEBioStudio2Dev/Linux/stage/bin
+scp cmbuild/lib/* repo:~/update2/FEBioStudio2Dev/Linux/stage/lib
+ssh repo "chmod +x update2/FEBioStudio2Dev/Linux/stage/bin/febio4"
