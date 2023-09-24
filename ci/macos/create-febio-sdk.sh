507c204edae776de38ff3b4fda6ad3119111c437
John Haynes <tundranerd@gmail.com>
2023-09-24 16:21:12 -0600

    Fix create-febio-sdk for macos

--- ci/macos/create-febio-sdk.sh
+++ ci/macos/create-febio-sdk.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 mkdir -p febio-sdk/{include,lib,bin}
-find . -name "*.h*" -type f -exec cp --parents -a {} ./febio-sdk/include/ \;
+# Gotta use rsync macos cp doesn't support --parent
+find . -name "*.h*" -type f -not -path "./infrastructure/*" -exec rsync -R -dir {} ./febio-sdk/include/ \;
 find cmbuild/lib -name "*" -type f -exec cp -a {} ./febio-sdk/lib/ \;
 find cmbuild/bin -name "*" -type f -exec cp -a {} ./febio-sdk/bin/ \;
