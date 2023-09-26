05e406828564f249e3e17595312b999d0590ac60
John Haynes <tundranerd@gmail.com>
2023-09-26 16:03:32 -0600

    Added openapi installer for MacOS

---
+++ infrastructure/common/macos/openapi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+TARGET_DIR="/tmp/intel"
+ONEAPI_FILE="m_BaseKit_p_2023.2.0.49398_offline"
+ONEAPI_DMG="$ONEAPI_FILE.dmg"
+ONEAPI_URI="https://registrationcenter-download.intel.com/akdlm/IRC_NAS/cd013e6c-49c4-488b-8b86-25df6693a9b7/$ONEAPI_DMG"
+curl --create-dirs -O --output-dir "$TARGET_DIR" "$ONEAPI_URI"
+hdiutil attach "$TARGET_DIR/$ONEAPI_DMG"
+pushd /Volumes/$ONEAPI_FILE/bootstrapper.app/Contents/MacOS/
+chmod +x ./install.sh
+arch -x86_64 ./install.sh --silent --eula accept
+popd
+hdiutil detach "/Volumes/$ONEAPI_FILE"
