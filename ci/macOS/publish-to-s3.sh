884110ced25f6c512b64e14bd1b35713d9999995
John Haynes <tundranerd@gmail.com>
2023-09-29 18:09:41 -0600

    WIP publish-to-s3 cleanup

--- ci/macOS/publish-to-s3.sh
+++ ci/macOS/publish-to-s3.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 chmod +x artifacts/febio/bin/febio4
-ci/common/linux/publish-to-s3.sh artifacts/febio
-ci/common/linux/publish-to-s3.sh artifacts/febio-sdk
+ci/common/linux/publish-to-s3.sh artifacts/febio4
+ci/common/linux/publish-to-s3.sh artifacts/febio4-sdk
