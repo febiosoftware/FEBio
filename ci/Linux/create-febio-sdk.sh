080cf39e355a1358f39a22b0bc37af6afc4b7b19
John Haynes <tundranerd@gmail.com>
2023-10-28 16:58:53 -0600

    WIP: Linux reusable workflow

--- ci/Linux/create-febio-sdk.sh
+++ ci/Linux/create-febio-sdk.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-mkdir -p febio-sdk/{include,lib,bin}
-find . -name "*.h*" -type f -exec cp --parents -a {} ./febio-sdk/include/ \;
-find cmbuild/lib -name "*" -type f -exec cp -a {} ./febio-sdk/lib/ \;
-find cmbuild/bin -name "*" -type f -exec cp -a {} ./febio-sdk/bin/ \;
+TARGET_DIR="${TARGET_DIR:-febio4-sdk}"
+mkdir -p ${TARGET_DIR}/{include,lib,bin}
+# Gotta use rsync macos cp doesn't support --parent
+find . -name "*.h*" -type f -not -path "./infrastructure/*" -not -path "./${TARGET_DIR}/*" -exec rsync -R -dir {} "./${TARGET_DIR}/include/" \;
+find cmbuild/lib -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/lib/" \;
+find cmbuild/bin -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/bin/" \;
