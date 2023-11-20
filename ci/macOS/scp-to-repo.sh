fa2d505b35744c2ba8221126a952c9e7db034e4c
John Haynes <tundranerd@gmail.com>
2023-11-20 13:54:23 -0700

    Push macOS artifacts to correct target paths

--- ci/macOS/scp-to-repo.sh
+++ ci/macOS/scp-to-repo.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-scp cmbuild/bin/* repo:~/update2/FEBioStudio2Dev/macOS/stage/bin
-scp cmbuild/lib/* repo:~/update2/FEBioStudio2Dev/macOS/stage/lib
-ssh repo "chmod +x update2/FEBioStudio2Dev/macOS/stage/bin/febio4"
+scp cmbuild/bin/* repo:~/update2/FEBioStudio2Dev/macOS/FEBioStudio.app/Contents/MacOS
+scp cmbuild/lib/* repo:~/update2/FEBioStudio2Dev/macOS/FEBioStudio.app/Contents/Frameworks
+ssh repo "chmod +x update2/FEBioStudio2Dev/macOS/FEBioStudio.app/Contents/MacOS/febio4"
