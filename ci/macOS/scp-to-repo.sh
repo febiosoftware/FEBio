5a200398e935d401bf05c088fdff36f19b779941
John Haynes <tundranerd@gmail.com>
2023-11-21 15:15:26 -0700

    Copy artifacts to correct staging folder

--- ci/macOS/scp-to-repo.sh
+++ ci/macOS/scp-to-repo.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-scp cmbuild/bin/* repo:~/update2/FEBioStudio2Dev/macOS/FEBioStudio.app/Contents/MacOS
-scp cmbuild/lib/* repo:~/update2/FEBioStudio2Dev/macOS/FEBioStudio.app/Contents/Frameworks
-ssh repo "chmod +x update2/FEBioStudio2Dev/macOS/FEBioStudio.app/Contents/MacOS/febio4"
+scp cmbuild/bin/* repo:~/update2/FEBioStudio2Dev/macOS/stage/FEBioStudio.app/Contents/MacOS
+scp cmbuild/lib/* repo:~/update2/FEBioStudio2Dev/macOS/stage/FEBioStudio.app/Contents/Frameworks
+ssh repo "chmod +x update2/FEBioStudio2Dev/macOS/stage/FEBioStudio.app/Contents/MacOS/febio4"
