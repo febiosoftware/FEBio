3092fcba0ebb69bb96c9eeff1020f3bf81440093
SteveMaas1978 <steve.maas@utah.edu>
2023-11-30 15:19:17 -0700

    Added check in reading Elements section to make sure element IDs are increasing.

--- FEBioXML/FEBioMeshSection4.cpp
+++ FEBioXML/FEBioMeshSection4.cpp
@@ -174,6 +174,12 @@ void FEBioMeshSection4::ParseElementSection(XMLTag& tag, FEBModel::Part* part)
 	dom->Reserve(10000);
 	vector<int> elemList; elemList.reserve(10000);
 
+	// keep track of largest ID
+	// we need to enforce that element IDs are increasing
+	// (This is currently only done for each domain. Need to modify this
+	// so it's done on the whole model.)
+	int maxID = -1;
+
 	// read element data
 	++tag;
 	do
@@ -182,6 +188,9 @@ void FEBioMeshSection4::ParseElementSection(XMLTag& tag, FEBModel::Part* part)
 
 		// get the element ID
 		tag.AttributeValue("id", el.id);
+
+		if ((maxID == -1) || (el.id > maxID)) maxID = el.id;
+		else throw XMLReader::InvalidAttributeValue(tag, "id");
 
 		// read the element data
 		tag.value(el.node, FEElement::MAX_NODES);
