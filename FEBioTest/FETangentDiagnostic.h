cf7d7ff07ee299025cb3c71dfcb1186928b5e87b
SteveMaas1978 <steve.maas@utah.edu>
2024-01-18 14:07:54 -0700

    Added additional scenarios for material test.

--- FEBioTest/FETangentDiagnostic.h
+++ FEBioTest/FETangentDiagnostic.h
@@ -58,6 +58,34 @@ private:
 };
 
 //-----------------------------------------------------------------------------
+class FETangentBiaxial : public FEDiagnosticScenario
+{
+public:
+	FETangentBiaxial(FEDiagnostic* pdia) : FEDiagnosticScenario(pdia) { m_strain = 0.0; }
+
+	bool Init() override;
+
+private:
+	double	m_strain;
+
+	DECLARE_FECORE_CLASS();
+};
+
+//-----------------------------------------------------------------------------
+class FETangentTriaxial : public FEDiagnosticScenario
+{
+public:
+	FETangentTriaxial(FEDiagnostic* pdia) : FEDiagnosticScenario(pdia) { m_strain = 0.0; }
+
+	bool Init() override;
+
+private:
+	double	m_strain;
+
+	DECLARE_FECORE_CLASS();
+};
+
+//-----------------------------------------------------------------------------
 //! The FETangentDiagnostic class tests the stiffness matrix implementation
 //! by comparing it to a numerical approximating of the derivative of the
 //! residual.
