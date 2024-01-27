76f1e317a031a37d38afd3aaab3048afdb01b364
John Haynes <tundranerd@gmail.com>
2024-01-27 14:25:20 -0700

    Create SDK wrapper for Windows

--- ci/Windows/create-sdk.sh
+++ ci/Windows/create-sdk.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
-TARGET_DIR="${TARGET_DIR:-febio4-sdk}"
-mkdir -p ${TARGET_DIR}/{include,lib,bin}
-find . -name "*.h*" -type f -not -path "./infrastructure/*" -not -path "./${TARGET_DIR}/*" -exec cp --parents -a {} "./${TARGET_DIR}/include/" \;
-find cmbuild/lib -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/lib/" \;
-find cmbuild/bin -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/bin/" \;
+parent_path=$(dirname $0)
+sh --login -i -c "${parent_path}/create-sdk-wrapped.sh"
