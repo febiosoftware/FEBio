87bdea3727864dbd9d44a3f4d5e992f496ed607e
John Haynes <tundranerd@gmail.com>
2024-03-02 13:58:19 -0700

    Chmod correct path for febio4

--- ci/Linux/publish-to-s3.sh
+++ ci/Linux/publish-to-s3.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 set -e
-chmod +x artifacts/febio/bin/febio4
+chmod +x artifacts/febio4/bin/febio4
 ci/common/linux/publish-to-s3.sh artifacts/febio4
 ci/common/linux/publish-to-s3.sh artifacts/febio4-sdk
