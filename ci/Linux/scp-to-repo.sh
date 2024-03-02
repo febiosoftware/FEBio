f1dc16bdc1ba2833c1134deb05560076f9624889
tundranerd <tundranerd@gmail.com>
2024-03-02 12:34:41 -0700

    Ci/pull request test (#4)
    * Linux push and pull_request workflows
    * OIDC for AWS

--- ci/Linux/scp-to-repo.sh
+++ ci/Linux/scp-to-repo.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 scp cmbuild/bin/* repo:~/update2/FEBioStudio2Dev/Linux/stage/bin
 scp cmbuild/lib/* repo:~/update2/FEBioStudio2Dev/Linux/stage/lib
 ssh repo "chmod +x update2/FEBioStudio2Dev/Linux/stage/bin/febio4"
--- ci/Linux/test.sh
+++ ci/Linux/test.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # Uncomment next line if not global on target machine
+set -e
 source "/opt/intel/oneapi/setvars.sh" --force
 ls -lr cmbuild
 chmod +x cmbuild/bin/febio4
