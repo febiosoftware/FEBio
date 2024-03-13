fd92ce0b196e372ebe072fd4792bdaf5f21a4384
michaelrossherron <michaelrossherron@gmail.com>
2024-03-13 15:51:44 -0600

    Reverted change in macOS build script

--- ci/macOS/build.sh
+++ ci/macOS/build.sh
@@ -4,5 +4,5 @@ git config --global --add safe.directory /FEBio
 source "/opt/intel/oneapi/setvars.sh" --force
 cmake . -B cmbuild -L -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13
 pushd cmbuild
-make -j8 #$(sysctl -n hw.ncpu)
+make -j $(sysctl -n hw.ncpu)
 #popd
