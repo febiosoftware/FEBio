7713072f90a391c6c2947e922615eea435e95405
John Haynes <tundranerd@gmail.com>
2024-04-26 15:09:16 -0600

    [skip ci] Verify Windows build and fast start

--- ci/Windows/test.bat
+++
@@ -1,3 +0,0 @@
-set PYTHON="%ONEAPI_ROOT%intelpython\latest\python"
-copy "%ONEAPI_ROOT%\compiler\latest\windows\redist\intel64_win\compiler\libiomp5md.dll" cmbuild\bin\Release
-%PYTHON% ./TestSuite/code/tools.py -r ./cmbuild/bin/Release/febio4.exe
