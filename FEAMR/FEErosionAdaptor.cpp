a5ea681df81e6736d008a265e74d645c68003767
SteveMaas1978 <steve.maas@utah.edu>
2024-08-15 09:45:57 -0600

    Implemented algorithm for growing surfaces when underlying elements are eroded.

--- FEAMR/FEErosionAdaptor.cpp
+++ FEAMR/FEErosionAdaptor.cpp
@@ -45,6 +45,7 @@ BEGIN_FECORE_CLASS(FEErosionAdaptor, FEMeshAdaptor)
 	ADD_PARAMETER(m_maxelem, "max_elems");
 	ADD_PARAMETER(m_nsort, "sort");
 	ADD_PARAMETER(m_bremoveIslands, "remove_islands");
+	ADD_PARAMETER(m_erodeSurfaces, "erode_surfaces")->setEnums("No\0Yes\0Grow\0");
 
 	ADD_PROPERTY(m_criterion, "criterion");
 END_FECORE_CLASS();
@@ -54,6 +55,8 @@ FEErosionAdaptor::FEErosionAdaptor(FEModel* fem) : FEMeshAdaptor(fem)
 	m_maxIters = -1;
 	m_maxelem = 0;
 	m_nsort = 0;
+	m_erodeSurfaces = SurfaceErodeOption::ERODE;
+
 	m_criterion = nullptr;
 	m_bremoveIslands = false;
 }
@@ -99,95 +102,43 @@ bool FEErosionAdaptor::Apply(int iteration)
 	// process the list
 	int nsize = selection.size();
 	if ((m_maxelem > 0) && (nsize > m_maxelem)) nsize = m_maxelem;
+	int deactivatedElements = 0;
 	for (int i = 0; i < nsize; ++i)
 	{
 		FEMeshAdaptorSelection::Item& it = selection[i];
 		FEElement& el = *mesh.FindElementFromID(it.m_elementId);
-		assert(el.isActive());
-		el.setInactive();
+		if (el.isActive())
+		{
+			el.setInactive();
+			deactivatedElements++;
+		}
 	}
-	feLog("\tDeactivated elements: %d\n", nsize);
+	feLog("\tDeactivated elements: %d\n", deactivatedElements);
+	if (deactivatedElements == 0) return false;
+
+	FEMeshTopo topo;
+	if (topo.Create(&mesh) == false)
+	{
+		feLogError("Failed building mesh topo.");
+		return false;
+	}
 
 	// remove any islands
-	if (m_bremoveIslands) RemoveIslands();
+	if (m_bremoveIslands) RemoveIslands(topo);
 
 	// if any nodes were orphaned, we need to deactivate them as well
-	int NN = mesh.Nodes();
-	vector<int> tag(NN, 0);
-	for (int i = 0; i < mesh.Domains(); ++i)
+	DeactivateOrphanedNodes();
+
+	// any facets attached to a eroded element will be eroded as well.
+	switch (m_erodeSurfaces)
 	{
-		FEDomain& dom = mesh.Domain(i);
-		int NE = dom.Elements();
-		for (int j = 0; j < NE; ++j)
-		{
-			FEElement& el = dom.ElementRef(j);
-			if (el.isActive())
-			{
-				int neln = el.Nodes();
-				for (int n = 0; n < neln; ++n) tag[el.m_node[n]] = 1;
-			}
-		}
+	case SurfaceErodeOption::DONT_ERODE: break;// don't do anything
+	case SurfaceErodeOption::ERODE: ErodeSurfaces(); break;
+	case SurfaceErodeOption::GROW : GrowErodedSurfaces(topo); break;
 	}
 
-	for (int i = 0; i < NN; ++i)
-	{
-		FENode& node = mesh.Node(i);
-		if (tag[i] == 0)
-		{
-			node.SetFlags(FENode::EXCLUDE);
-			int ndofs = node.dofs();
-			for (int j = 0; j < ndofs; ++j)
-				node.set_inactive(j);
-		}
-	}
-
-	// any facets attached to a eroded element will be eroded as well. 
-	for (int i = 0; i < mesh.Surfaces(); ++i)
-	{
-		int erodedFaces = 0;
-		FESurface& surf = mesh.Surface(i);
-		for (int j = 0; j < surf.Elements(); ++j)
-		{
-			FESurfaceElement& face = surf.Element(j);
-			FEElement* pe = face.m_elem[0]; assert(pe);
-			if (pe && (pe->isActive() == false))
-			{
-				face.setInactive();
-				erodedFaces++;
-			}
-		}
-		if (erodedFaces != 0) surf.Init();
-	}
-
-	// remove any linear constraints of exclude nodes
-	FELinearConstraintManager& LCM = fem.GetLinearConstraintManager();
-	for (int j = 0; j < LCM.LinearConstraints();)
-	{
-		FELinearConstraint& lc = LCM.LinearConstraint(j);
-		if (mesh.Node(lc.GetParentNode()).HasFlags(FENode::EXCLUDE))
-		{
-			LCM.RemoveLinearConstraint(j);
-		}
-		else ++j;
-	}
-
-	// also remove any linear constraints that have excluded child nodes
-	for (int j = 0; j < LCM.LinearConstraints();)
-	{
-		FELinearConstraint& lc = LCM.LinearConstraint(j);
-
-		bool del = false;
-		int n = lc.Size();
-		for (int k = 0; k < n; ++k)
-		{
-			if (mesh.Node(lc.GetChildDof(k).node).HasFlags(FENode::EXCLUDE))
-			{
-				del = true;
-				break;
-			}
-		}
-		if (del) LCM.RemoveLinearConstraint(j); else ++j;
-	}
+	// remove any linear constraints of excluded nodes
+	UpdateLinearConstraints();
 
 	// update model
 	UpdateModel();
@@ -195,16 +146,10 @@ bool FEErosionAdaptor::Apply(int iteration)
 	return (nsize != 0);
 }
 
-void FEErosionAdaptor::RemoveIslands()
+void FEErosionAdaptor::RemoveIslands(FEMeshTopo& topo)
 {
 	FEModel& fem = *GetFEModel();
 	FEMesh& mesh = fem.GetMesh();
-
-	FEMeshTopo topo;
-	if (topo.Create(&mesh) == false)
-	{
-		feLogError("Failed removing islands.");
-	}
 
 	int NE = topo.Elements();
 	vector<int> tag(NE, -1);
@@ -280,5 +225,213 @@ void FEErosionAdaptor::RemoveIslands()
 				}
 			}
 		}
+	}
+}
+
+void FEErosionAdaptor::DeactivateOrphanedNodes()
+{
+	FEModel& fem = *GetFEModel();
+	FEMesh& mesh = fem.GetMesh();
+
+	int NN = mesh.Nodes();
+	vector<int> tag(NN, 0);
+	for (int i = 0; i < mesh.Domains(); ++i)
+	{
+		FEDomain& dom = mesh.Domain(i);
+		int NE = dom.Elements();
+		for (int j = 0; j < NE; ++j)
+		{
+			FEElement& el = dom.ElementRef(j);
+			if (el.isActive())
+			{
+				int neln = el.Nodes();
+				for (int n = 0; n < neln; ++n) tag[el.m_node[n]] = 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < NN; ++i)
+	{
+		FENode& node = mesh.Node(i);
+		if (tag[i] == 0)
+		{
+			node.SetFlags(FENode::EXCLUDE);
+			int ndofs = node.dofs();
+			for (int j = 0; j < ndofs; ++j)
+				node.set_inactive(j);
+		}
+	}
+}
+
+void FEErosionAdaptor::ErodeSurfaces()
+{
+	FEModel& fem = *GetFEModel();
+	FEMesh& mesh = fem.GetMesh();
+
+	for (int i = 0; i < mesh.Surfaces(); ++i)
+	{
+		int erodedFaces = 0;
+		FESurface& surf = mesh.Surface(i);
+		for (int j = 0; j < surf.Elements(); ++j)
+		{
+			FESurfaceElement& face = surf.Element(j);
+			FEElement* pe = face.m_elem[0]; assert(pe);
+			if (pe && (pe->isActive() == false))
+			{
+				face.setInactive();
+				erodedFaces++;
+			}
+		}
+		if (erodedFaces != 0) surf.Init();
+	}
+}
+
+void FEErosionAdaptor::GrowErodedSurfaces(FEMeshTopo& topo)
+{
+	FEModel& fem = *GetFEModel();
+	FEMesh& mesh = fem.GetMesh();
+
+	for (int i = 0; i < mesh.Surfaces(); ++i)
+	{
+		FESurface& surf = mesh.Surface(i);
+
+		// do a search to see if we need to modify this surface
+		bool doErode = false;
+		for (int j = 0; j < surf.Elements(); ++j)
+		{
+			FESurfaceElement& face = surf.Element(j);
+			if (face.isActive())
+			{
+				FEElement* pe = face.m_elem[0]; assert(pe);
+				if (pe && (pe->isActive() == false))
+				{
+					doErode = true;
+					break;
+				}
+			}
+		}
+
+		if (doErode)
+		{
+			std::vector<FEFaceList::FACE> newFaces;
+			newFaces.reserve(surf.Elements());
+			int erodedFaces = 0;
+			for (int j = 0; j < surf.Elements(); ++j)
+			{
+				FESurfaceElement& face = surf.Element(j);
+				if (face.isActive())
+				{
+					FEElement* pe = face.m_elem[0]; assert(pe);
+					if (pe && (pe->isActive() == false))
+					{
+						int id = topo.GetElementIndexFromID(pe->GetID());
+						std::vector<FEElement*> nbrList = topo.ElementNeighborList(id);
+						for (int k = 0; k < nbrList.size(); ++k)
+						{
+							FEElement* pk = nbrList[k];
+							if ((pk != nullptr) && pk->isActive())
+							{
+								int node[FEElement::MAX_NODES] = { 0 };
+								int nf = pe->GetFace(k, node);
+
+								// Note that we invert the element to
+								// make sure the new face is outward pointing
+								FEFaceList::FACE newFace;
+								newFace.ntype = nf;
+								for (int l = 0; l < nf; ++l)
+									newFace.node[l] = node[nf - 1 - l];
+
+								if (!newFace.IsEqual(face.m_node.data()))
+									newFaces.push_back(newFace);
+							}
+						}
+
+						face.setInactive();
+						erodedFaces++;
+					}
+					else
+					{
+						FEFaceList::FACE newFace;
+						newFace.ntype = face.Nodes();
+						for (int k = 0; k < face.Nodes(); ++k)
+							newFace.node[k] = face.m_node[k];
+						newFaces.push_back(newFace);
+					}
+				}
+			}
+			feLog("eroded faces: %d", erodedFaces);
+			if (erodedFaces != 0)
+			{
+				int faces = newFaces.size();
+				surf.Create(faces);
+				for (int j = 0; j < faces; ++j)
+				{
+					FEFaceList::FACE& f = newFaces[j];
+					FESurfaceElement& face = surf.Element(j);
+					face.setActive();
+
+					if (f.ntype == 3)
+						face.SetType(FE_TRI3G3);
+					else if (f.ntype == 4)
+						face.SetType(FE_QUAD4G4);
+
+					for (int k = 0; k < f.ntype; ++k)
+						face.m_node[k] = f.node[k];
+				}
+
+				surf.CreateMaterialPointData();
+				surf.Init();
+
+				// also update the facet set if the surface has one
+				FEFacetSet* fset = surf.GetFacetSet();
+				if (fset)
+				{
+					fset->Create(surf);
+				}
+			}
+
+			feLog("New surface:\n");
+			for (int k = 0; k < surf.Elements(); ++k)
+			{
+				FESurfaceElement& face = surf.Element(k);
+				int* n = face.m_node.data();
+				feLog("\t%d %d %d %d\n", n[0], n[1], n[2], n[3]);
+			}
+		}
+	}
+}
+
+void FEErosionAdaptor::UpdateLinearConstraints()
+{
+	FEModel& fem = *GetFEModel();
+	FEMesh& mesh = fem.GetMesh();
+
+	FELinearConstraintManager& LCM = fem.GetLinearConstraintManager();
+	for (int j = 0; j < LCM.LinearConstraints();)
+	{
+		FELinearConstraint& lc = LCM.LinearConstraint(j);
+		if (mesh.Node(lc.GetParentNode()).HasFlags(FENode::EXCLUDE))
+		{
+			LCM.RemoveLinearConstraint(j);
+		}
+		else ++j;
+	}
+
+	// also remove any linear constraints that have excluded child nodes
+	for (int j = 0; j < LCM.LinearConstraints();)
+	{
+		FELinearConstraint& lc = LCM.LinearConstraint(j);
+
+		bool del = false;
+		int n = lc.Size();
+		for (int k = 0; k < n; ++k)
+		{
+			if (mesh.Node(lc.GetChildDof(k).node).HasFlags(FENode::EXCLUDE))
+			{
+				del = true;
+				break;
+			}
+		}
+		if (del) LCM.RemoveLinearConstraint(j); else ++j;
 	}
 }
