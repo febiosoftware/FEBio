f5914ff7ff28905a7b92c58705f2af08f0003cf0
michaelrossherron <michaelrossherron@gmail.com>
2025-01-15 15:20:31 -0700

    Updated ci scripts to allow for dev releases on other branches. [skip ci]

--- ci/Linux/scp-to-repo.sh
+++ ci/Linux/scp-to-repo.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
+
+REMOTE_PATH="update2/FEBioStudio2Dev/Linux/stage"
+if [ $# == 1 ] && [ "$1" != "develop" ]; then
+    REMOTE_PATH="update2/FEBioStudio2Dev/branches/$1/Linux/stage"
+fi
+
+
 set -e
-scp cmbuild/bin/* repo:~/update2/FEBioStudio2Dev/Linux/stage/bin
-scp cmbuild/lib/* repo:~/update2/FEBioStudio2Dev/Linux/stage/lib
-ssh repo "chmod +x update2/FEBioStudio2Dev/Linux/stage/bin/febio4"
+scp cmbuild/bin/* repo:~/$REMOTE_PATH/bin
+scp cmbuild/lib/* repo:~/$REMOTE_PATH/lib
+ssh repo "chmod +x $REMOTE_PATH/bin/febio4"
 
 
 if [ -f testLogs/Logs/* ]; then
@@ -10,9 +17,9 @@ if [ -f testLogs/Logs/* ]; then
 fi
 
 if [ -f ChemArtifacts/lib/* ]; then
-    scp ChemArtifacts/lib/* repo:~/update2/FEBioStudio2Dev/Linux/stage/lib
+    scp ChemArtifacts/lib/* repo:~/$REMOTE_PATH/lib
 fi
 
 if [ -f HeatArtifacts/lib/* ]; then
-    scp HeatArtifacts/lib/* repo:~/update2/FEBioStudio2Dev/Linux/stage/lib
+    scp HeatArtifacts/lib/* repo:~/$REMOTE_PATH/lib
 fi
