fa8cbfc3748788d17f4513b0b1ea1d2dca121582
michaelrossherron <michaelrossherron@gmail.com>
2025-01-16 16:21:55 -0700

    Updated CMakeLists and macOS CI cmake script to fix issues with omp header

--- ci/macOS/cmake.sh
+++ ci/macOS/cmake.sh
@@ -3,4 +3,5 @@ cmake . -B cmbuild -L \
 	-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
 	-DUSE_MMG=ON \
 	-DUSE_LEVMAR=ON \
-	-DUSE_HYPRE=ON
+	-DUSE_HYPRE=ON \
+    -DOMP_INC=/Users/gitRunner/local/x86_64/homebrew/opt/libomp/include
