a30d566d99d7a15e4ae95d0d925b3ccd8024e781
SteveMaas1978 <steve.maas@utah.edu>
2025-08-13 10:55:07 -0600

    Implemented fftw-based blurring algorithm.

--- FEImgLib/image_tools.h
+++ FEImgLib/image_tools.h
@@ -8,3 +8,6 @@ FEIMGLIB_API void blur_image(Image& trg, Image& src, float d);
 
 FEIMGLIB_API void fftblur_2d(Image& trg, Image& src, float d);
 FEIMGLIB_API void fftblur_3d(Image& trg, Image& src, float d);
+
+FEIMGLIB_API void fftw_blur_2d(Image& trg, Image& src, float d);
+FEIMGLIB_API void fftw_blur_3d(Image& trg, Image& src, float d);
