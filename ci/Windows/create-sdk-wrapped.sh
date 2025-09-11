4c5012ab88b570fae5094626b14af4f5ffdf57e8
michaelrossherron <michaelrossherron@gmail.com>
2025-09-11 14:37:09 -0600

    Reverted windows create-sdk changes. Would not run properly. [skip ci]

--- ci/Windows/create-sdk-wrapped.sh
+++ ci/Windows/create-sdk-wrapped.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 TARGET_DIR="${TARGET_DIR:-febio4-sdk}"
 mkdir -p ${TARGET_DIR}/{include,lib,bin}
-
-find . \( -name "*.h" -o -name "*.hpp" \) -type f -not -path "./${TARGET_DIR}/*" -exec cp --parents -a -t "./${TARGET_DIR}/include/" {} +
-find cmbuild/lib -type f -exec cp -a -t "./${TARGET_DIR}/lib/" {} +
-find cmbuild/bin -type f -exec cp -a -t "./${TARGET_DIR}/bin/" {} +
+find . -name "*.h*" -type f -not -path "./infrastructure/*" -not -path "./${TARGET_DIR}/*" -exec cp --parents -a {} "./${TARGET_DIR}/include/" \;
+find cmbuild/lib -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/lib/" \;
+find cmbuild/bin -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/bin/" \;
