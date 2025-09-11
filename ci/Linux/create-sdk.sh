416e48d3faa73e5902d49185c4932e0fe912d2ca
michaelrossherron <michaelrossherron@gmail.com>
2025-09-11 13:36:17 -0600

    Updated create-sdk script to fix slow execution.

--- ci/Linux/create-sdk.sh
+++ ci/Linux/create-sdk.sh
@@ -2,7 +2,8 @@
 set -e
 TARGET_DIR="${TARGET_DIR:-febio4-sdk}"
 mkdir -p ${TARGET_DIR}/{include,lib,bin}
-# Gotta use rsync macos cp doesn't support --parent
-find . -name "*.h*" -type f -not -path "./infrastructure/*" -not -path "./${TARGET_DIR}/*" -exec rsync -R -dir {} "./${TARGET_DIR}/include/" \;
-find cmbuild/lib -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/lib/" \;
-find cmbuild/bin -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/bin/" \;
+
+rsync -a --prune-empty-dirs --include="*.h" --include="*.hpp" --exclude="*" ./ "./${TARGET_DIR}/include/"
+find cmbuild/lib -type f -exec cp -at "./${TARGET_DIR}/lib/" {} +
+find cmbuild/bin -type f -exec cp -at "./${TARGET_DIR}/bin/" {} +
+
--- ci/Windows/create-sdk-wrapped.sh
+++ ci/Windows/create-sdk-wrapped.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 TARGET_DIR="${TARGET_DIR:-febio4-sdk}"
 mkdir -p ${TARGET_DIR}/{include,lib,bin}
-find . -name "*.h*" -type f -not -path "./infrastructure/*" -not -path "./${TARGET_DIR}/*" -exec cp --parents -a {} "./${TARGET_DIR}/include/" \;
-find cmbuild/lib -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/lib/" \;
-find cmbuild/bin -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/bin/" \;
+rsync -a --prune-empty-dirs --include="*.h" --include="*.hpp" --exclude="*" ./ "./${TARGET_DIR}/include/"
+find cmbuild/lib -type f -exec cp -at "./${TARGET_DIR}/lib/" {} +
+find cmbuild/bin -type f -exec cp -at "./${TARGET_DIR}/bin/" {} +
