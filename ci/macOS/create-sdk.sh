14c2c65ae921133a09289cdfb2c931b1860a4b7e
michaelrossherron <michaelrossherron@gmail.com>
2025-09-11 14:39:03 -0600

    Fixed issue with create-sdk script on macOS [skip ci]

--- ci/macOS/create-sdk.sh
+++ ci/macOS/create-sdk.sh
@@ -3,5 +3,5 @@ TARGET_DIR="${TARGET_DIR:-febio4-sdk}"
 mkdir -p ${TARGET_DIR}/{include,lib,bin}
 
 rsync -a --prune-empty-dirs --include="*/" --include="*.h" --include="*.hpp" --exclude="*" ./ "./${TARGET_DIR}/include/"
-find cmbuild/lib -type f -exec cp -at "./${TARGET_DIR}/lib/" {} +
-find cmbuild/bin -type f -exec cp -at "./${TARGET_DIR}/bin/" {} +
+find cmbuild/lib -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/lib/" \;
+find cmbuild/bin -name "*" -type f -exec cp -a {} "./${TARGET_DIR}/bin/" \;
