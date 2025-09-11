6c510656ad9192b948423c71cc7be3e638879681
michaelrossherron <michaelrossherron@gmail.com>
2025-09-11 14:02:59 -0600

    Fixed issues with create-sdk changes

--- ci/Windows/create-sdk.sh
+++ ci/Windows/create-sdk.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
-parent_path=$(dirname $0)
-sh --login -i -c "${parent_path}/create-sdk-wrapped.sh"
+TARGET_DIR="${TARGET_DIR:-febio4-sdk}"
+mkdir -p ${TARGET_DIR}/{include,lib,bin}
+
+find . \( -name "*.h" -o -name "*.hpp" \) -type f -not -path "./${TARGET_DIR}/*" -exec cp --parents -a -t "./${TARGET_DIR}/include/" {} +
+find cmbuild/lib -type f -exec cp -a -t "./${TARGET_DIR}/lib/" {} +
+find cmbuild/bin -type f -exec cp -a -t "./${TARGET_DIR}/bin/" {} +
--- ci/macOS/create-sdk.sh
+++ ci/macOS/create-sdk.sh
@@ -2,6 +2,6 @@
 TARGET_DIR="${TARGET_DIR:-febio4-sdk}"
 mkdir -p ${TARGET_DIR}/{include,lib,bin}
 
-rsync -a --prune-empty-dirs --include="*.h" --include="*.hpp" --exclude="*" ./ "./${TARGET_DIR}/include/"
-find cmbuild/lib -type f -exec cp -at "./${TARGET_DIR}/lib/" {} +
-find cmbuild/bin -type f -exec cp -at "./${TARGET_DIR}/bin/" {} +
+find . \( -name "*.h" -o -name "*.hpp" \) -type f -not -path "./${TARGET_DIR}/*" -exec cp --parents -a -t "./${TARGET_DIR}/include/" {} +
+find cmbuild/lib -type f -exec cp -a -t "./${TARGET_DIR}/lib/" {} +
+find cmbuild/bin -type f -exec cp -a -t "./${TARGET_DIR}/bin/" {} +
