5c3568740c492766a733ceb4ab8c5cf327cba558
SteveMaas1978 <steve.maas@utah.edu>
2025-09-30 10:32:53 -0600

    Fixed issue in elasticity calculation in several materials.

--- FEBioMech/FEMuscleMaterial.cpp
+++ FEBioMech/FEMuscleMaterial.cpp
@@ -238,9 +238,6 @@ tens4ds FEMuscleMaterial::DevTangent(FEMaterialPoint& mp)
 	mat3d &F = pt.m_F;
 	double J = pt.m_J;
 
-	// deviatoric cauchy-stress, trs = trace[s]/3
-	mat3ds devs = pt.m_s.dev();
-
 	// get the local coordinate systems
 	mat3d Q = GetLocalCS(mp);
 
@@ -463,6 +460,11 @@ tens4ds FEMuscleMaterial::DevTangent(FEMaterialPoint& mp)
 	// let's put it all together
 	// cw
 	tens4ds cw =  IxI*((4.0/(9.0*J))*(CW2CCC)) + W2CC*(4/J) - dyad1s(WCCC, ID)*(4.0/(3.0*J));
+
+	// deviatoric Cauchy-stress
+	mat3ds ABA = dyads(a, Ba);
+	mat3ds T = B * (W1 + W2 * I1) - B2 * W2 + AxA * (I4 * W4) + ABA * (I4 * W5);
+	mat3ds devs = T.dev() * (2.0 / J);
 
 	// elasticity tensor
 	tens4ds c = dyad1s(devs, ID)*(-2.0/3.0) + (I - IxI/3.0)*(4.0*WCC/(3.0*J)) + cw;
