7d4fdb9e13163022eec842671c87d5af916f3b36
michaelrossherron <michaelrossherron@gmail.com>
2025-10-09 12:43:32 -0600

    Explicitly defined all cmake options in build scripts

--- ci/macOS/cmake.sh
+++ ci/macOS/cmake.sh
@@ -1,7 +1,11 @@
 cmake . -B cmbuild -L \
 	-DCMAKE_OSX_ARCHITECTURES="x86_64" \
 	-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
-	-DUSE_MMG=ON \
-	-DUSE_LEVMAR=ON \
+	-DSET_DEVCOMMIT=ON \
+	-DUSE_FFTW=ON \
 	-DUSE_HYPRE=ON \
+	-DUSE_LEVMAR=ON \
+	-DUSE_MKL=ON \
+	-DUSE_MMG=ON \
+	-DUSE_ZLIB=ON \
     -DOMP_INC=/Users/gitRunner/local/x86_64/homebrew/opt/libomp/include
