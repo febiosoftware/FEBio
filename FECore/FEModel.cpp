95333cfa55780828125ba213314a9dfd9d2c13ad
SteveMaas1978 <steve.maas@utah.edu>
2026-03-04 17:08:32 -0700

    Fixed bug in GetMeshParameter when node is references via fromId function.

--- FECore/FEModel.cpp
+++ FECore/FEModel.cpp
@@ -1704,7 +1704,7 @@ FEParamValue FEModel::GetMeshParameter(const ParamString& paramString)
 			ParamString fnc = next.next();
 			if (fnc == "fromId")
 			{
-				nid = fnc.Index();
+				nid = fnc.ID();
 				node = mesh.FindNodeFromID(nid);
 				next = next.next();
 			}
