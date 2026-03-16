74791e1b126f7c9b5f25540c5bb42f85021560b3
SteveMaas1978 <steve.maas@utah.edu>
2026-03-16 14:54:07 -0600

    Node numbers are now validated when building edges from the input file.

--- FEBioXML/FEBModel.cpp
+++ FEBioXML/FEBModel.cpp
@@ -543,7 +543,16 @@ bool FEBModel::BuildPart(FEModel& fem, Part& part, bool buildDomains, const Tran
 
 			seg.ntype = edge.ntype;
 			int nn = edge.ntype;	// we assume that the type also identifies the number of nodes
-			for (int n = 0; n < nn; ++n) seg.node[n] = NLT[edge.node[n] - noff];
+			for (int n = 0; n < nn; ++n)
+			{
+				int nid = edge.node[n] - noff;
+				if (nid < 0 || nid >= NLT.size() || NLT[nid] < 0)
+				{
+					feLogErrorEx(&fem, "Invalid node index %d in edge set %s", edge.node[n], edgeSet->Name().c_str());
+					return false;
+				}
+				seg.node[n] = NLT[nid];
+			}
 		}
 
 		// add it to the mesh
