219bd77f2338c2a1f17e0962ef91b140fdc784d8
SteveMaas1978 <steve.maas@utah.edu>
2026-04-21 14:07:55 -0600

    Expanded FEValueType and FEValue to include all currently supported febcode types. Added comparison operators for ScriptContext.

--- FECore/FEScriptedBehavior.h
+++ FECore/FEScriptedBehavior.h
@@ -8,13 +8,17 @@
 class FEMaterialPoint;
 class FEModel;
 
+// NOTE: Don't change the order of these values!
+// Also make sure this list matches the union in FEValue!
 enum class FEValueType
 {
 	Invalid,
 	Bool,
 	Int,
 	Double,
+	Vec2d,
 	Vec3d,
+	Mat2d,
 	Mat3d
 };
 
@@ -25,7 +29,9 @@ struct FEValue
 		bool b;
 		int i;
 		double d;
+		vec2d v2;
 		vec3d v3;
+		mat2d m2;
 		mat3d m3;
 	};
 
@@ -39,7 +45,9 @@ struct FEValue
 		case FEValueType::Bool  : b = other.b; break;
 		case FEValueType::Int   : i = other.i; break;
 		case FEValueType::Double: d = other.d; break;
+		case FEValueType::Vec2d : v2 = other.v2; break;
 		case FEValueType::Vec3d : v3 = other.v3; break;
+		case FEValueType::Mat2d : m2 = other.m2; break;
 		case FEValueType::Mat3d : m3 = other.m3; break;
 		}
 	}
@@ -52,7 +60,9 @@ struct FEValue
 		case FEValueType::Bool: b = other.b; break;
 		case FEValueType::Int: i = other.i; break;
 		case FEValueType::Double: d = other.d; break;
+		case FEValueType::Vec2d: v2 = other.v2; break;
 		case FEValueType::Vec3d: v3 = other.v3; break;
+		case FEValueType::Mat2d: m2 = other.m2; break;
 		case FEValueType::Mat3d: m3 = other.m3; break;
 		}
 		return *this;
@@ -61,7 +71,9 @@ struct FEValue
 	void operator = (bool v) { type = FEValueType::Bool; b = v; }
 	void operator = (int v) { type = FEValueType::Int; i = v; }
 	void operator = (double v) { type = FEValueType::Double; d = v; }
+	void operator = (const vec2d& v) { type = FEValueType::Vec2d; v2 = v; }
 	void operator = (const vec3d& v) { type = FEValueType::Vec3d; v3 = v; }
+	void operator = (const mat2d& v) { type = FEValueType::Mat2d; m2 = v; }
 	void operator = (const mat3d& v) { type = FEValueType::Mat3d; m3 = v; }
 };
 
@@ -80,6 +92,24 @@ struct FECORE_API ScriptContext
 	void addVariable(const std::string& name, FEValueType type, bool differentiable)
 	{
 		variables.push_back({ name, type, differentiable });
+	}
+
+	bool operator == (const ScriptContext& other) const
+	{
+		if (returnType != other.returnType) return false;
+		if (variables.size() != other.variables.size()) return false;
+		for (size_t i = 0; i < variables.size(); ++i)
+		{
+			if (variables[i].name != other.variables[i].name) return false;
+			if (variables[i].type != other.variables[i].type) return false;
+			if (variables[i].differentiable != other.variables[i].differentiable) return false;
+		}
+		return true;
+	}
+
+	bool operator != (const ScriptContext& other) const
+	{
+		return !(*this == other);
 	}
 };
 
