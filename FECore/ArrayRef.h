b5df32d82c6402738597ca6f4edf8833eb979359
SteveMaas1978 <steve.maas@utah.edu>
2026-09-25 06:44:06 -0600

    Removed CUDA annotations from ArrayRef

--- FECore/ArrayRef.h
+++ FECore/ArrayRef.h
@@ -2,15 +2,6 @@
 #include <memory>
 #include <vector>
 
-#if defined(__CUDACC__)
-	#define FECORE_CUDA_HOST_DEVICE __host__ __device__
-	#define FECORE_CUDA_HOST        __host__
-	#define FECORE_CUDA_DEVICE      __device__
-#else
-	#define FECORE_CUDA_HOST_DEVICE
-	#define FECORE_CUDA_HOST
-	#define FECORE_CUDA_DEVICE
-#endif
 
 namespace fecore {
 
@@ -18,19 +9,15 @@ namespace fecore {
 	class ArrayRef
 	{
 	public:
-		FECORE_CUDA_HOST_DEVICE
-			ArrayRef(T* data, size_t size) : m_data(data), m_size(size) {}
+		ArrayRef(T* data, size_t size) : m_data(data), m_size(size) {}
 
 		ArrayRef(std::vector<T>& v) : m_data(v.data()), m_size(v.size()) {}
 
-		FECORE_CUDA_HOST_DEVICE
-			size_t size() const { return m_size; }
+		size_t size() const { return m_size; }
 
-		FECORE_CUDA_HOST_DEVICE
-			T& operator[](size_t i) const { return m_data[i]; }
+		T& operator[](size_t i) const { return m_data[i]; }
 
-		FECORE_CUDA_HOST_DEVICE
-			T* data() const { return m_data; }
+		T* data() const { return m_data; }
 
 	private:
 		T* m_data = nullptr;
@@ -41,21 +28,17 @@ namespace fecore {
 	class ConstArrayRef
 	{
 	public:
-		FECORE_CUDA_HOST_DEVICE
-			ConstArrayRef(const T* data, size_t size) : m_data(data), m_size(size) {}
+		ConstArrayRef(const T* data, size_t size) : m_data(data), m_size(size) {}
 
 		ConstArrayRef(const std::vector<T>& v) : m_data(v.data()), m_size(v.size()) {}
 
 		ConstArrayRef(const ArrayRef<T>& arr) : m_data(arr.data()), m_size(arr.size()) {}
 
-		FECORE_CUDA_HOST_DEVICE
-			size_t size() const { return m_size; }
+		size_t size() const { return m_size; }
 
-		FECORE_CUDA_HOST_DEVICE
-			const T& operator[](size_t i) const { return m_data[i]; }
+		const T& operator[](size_t i) const { return m_data[i]; }
 
-		FECORE_CUDA_HOST_DEVICE
-			const T* data() const { return m_data; }
+		const T* data() const { return m_data; }
 
 	private:
 		const T* m_data = nullptr;
